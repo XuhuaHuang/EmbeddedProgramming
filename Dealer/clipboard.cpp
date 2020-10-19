331c5186d5da7fc22076b05ff1c7b1bfda5db304
Xuhua Andy Huang <71360506+XuhuaHuang@users.noreply.github.com>
2020-10-19 10:41:03 -0400

    Create clipboard.cpp

---
+++ Dealer/clipboard.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+// Xuhua Huang 1879700
+
+class Card
+{
+	private:
+		int face;
+		int suit;
+		vector<string>  allFaces;
+		vector<allsuits>
+	
+	public:
+	
+};
