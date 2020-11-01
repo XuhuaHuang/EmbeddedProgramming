34e8021891bd78f5fb0cd906cc3a9e4d825dbac9
Xuhua Andy Huang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-01 11:20:02 -0500

    Spilt a three  digits number

---
+++ Fun/fun_spilt_digits.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void)
+{
+	int threeDigit;
+	cout << "Please enter a three digit number: ";
+	cin >> threeDigit;
+
+	cout << "The most significant digit is: " << threeDigit / 100 << endl;
+	cout << "The digit at the middle is: " << (threeDigit % 100) / 10 << endl;
+	cout << "THe least significant digit is: " << threeDigit % 10 << endl;
+
+	return 0;
+}
