f118a31695b17320a41cfcab9f8172e7928e2b08
Xuhua Andy Huang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-02 23:33:36 -0500

    Look at line 25, std::fixed

---
+++ Fun/fun_hex_octal_string_brief.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <float.h>
+#include <string> // grants access to string class
+#include <cmath>
+
+using namespace std;
+
+//using std::cout;
+//using std::cin;
+
+int main (void)
+{
+	// boolean value
+	bool life_is_good = true;
+	cout << boolalpha << life_is_good << endl;
+	if (life_is_good) // brackets evaluate the condition and feedback either true or false
+	cout << "This is Ture." << endl;
+	
+	
+	//float and double
+	float a = 0;
+	double b;
+	long double c;
+	
+	cout << std::fixed << a << std::endl;
+	// std::fixed gets rid of scientific notation
+	
+	cout << LDBL_DIG << std::endl; 
+	// this line is evaluating up to how many digits we can trust on these vaariables
+	// 6 for float, 15 for double, 18 for long double
+	
+	// string
+	string greeting = "What's up?";
+	cout << greeting.find_first_of("aeiou") << endl; // string member modifier or methods
+	/*	if not found, npos == -1
+		however, it's an unsigned integer 
+		negative value makes it go 1 lower to a trenmandously big
+	*/
+	if (greeting.find_first_of("!") == -1) std::cout << "NOT FOUND!" << endl;
+	
+	if (greeting.compare("What's up?") == 0) std::cout << "EQUALS!" << endl;
+	// compare funtion returns a bool 0 to indicate found
+
+	// hex and octal int
+	int x = 0x30; // 0x indicates this is a hex int
+	// second digit is 1 6, 16 times 3 gives 48 in decimal
+
+	int x = 030; // 0 indicates an octal int
+	// second digit is 8^1 = 8. 8 * 3 = 24
+	
+	return 0;
+}
