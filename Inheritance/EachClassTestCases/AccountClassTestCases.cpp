c7c4bd317a74726ddf7abf4ef5fdf8e04bda82f3
Xuhua Andy Huang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-03 18:35:51 -0500

    With colour changing code.

--- Inheritance/EachClassTestCases/AccountClassTestCases.cpp
+++ Inheritance/EachClassTestCases/AccountClassTestCases.cpp
@@ -1,15 +1,21 @@
 /*
 * 1879700 Xuhua Huang
 * Inheritance Lab Main File
-* 
+*
 * Contains:
 * main function
+* - Test cases for each class
+*
+* Notice: when executing, "cout" are printed without signatures
+* "cout" statements with signature "[fn]" come from functions
+* If not specified, default constructors are called
 */
 
 // built-in library:
 #include <iostream>
 #include <math.h>
 #include <string>
+#include <Windows.h> // for changing colours 
 //#include <iomanip>
 //#include <float.h>
 
@@ -27,17 +33,19 @@ using namespace std;
 int main(void)
 {
 	cout.precision(2); // force double digits with using "std::fixed" in this scope i.e. main function
-	
+
 	// Test Account Class
-	cout << "Testing 'Account' class..." << endl;
-	
+	//system("color 0A"); // change text color to green using <Windows.h> built-in function system(color XY);
+	cout << "\033[1;32m"; // change colour to green
+	cout << "Testing \"Account\" class..." << endl;
+
 	cout << "\nCreating a new account..." << endl;
 	Account myAccount1; // creating an account
 	cout << "\nCurrent balance is at: $ " << fixed << myAccount1.getBalance() << endl; // $0
-	
+
 	cout << "\nCreating a new account with initial balance of $100.10..." << endl;
 	Account my100Account(100.10); // 100 is the 'initBalance'
-	cout << "\nNew account created with overloaded constructor.\n[main]Account balance is at: " << fixed << my100Account.getBalance() << endl; // $100
+	cout << "\nNew account created with overloaded constructor.\nAccount balance is at: " << fixed << my100Account.getBalance() << endl; // $100
 
 	cout << "\nSetting new balance to $200.20..." << endl;
 	myAccount1.setBalance(200.20);
@@ -54,9 +62,9 @@ int main(void)
 	cout << "\nAttempting to withdraw $300.30..." << endl;
 	myAccount1.debit(300.30); // not enough funds to withdraw, print error only
 	cout << "\nKnown no changes should have been made to myAccount1. Balance remains: $ " << fixed << myAccount1.getBalance() << endl;
-	
-	cout << "Test cases for class \"Account\" end here." << endl;
 
+	cout << "\nTest cases for class \"Account\" end here.\n" << endl;
+	cout << "\033[0m"; // reset colour to white
 
 	return 0;
 }
