451db451ec61aadda5d2a91cb2836003cbc7472c
Xuhua Andy Huang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-06 21:42:24 -0500

    Added Fibonacci series. Duplicated from 'fun_fibonacci_series.cpp'

--- Fun/fun_math_with_loops.cpp
+++ Fun/fun_math_with_loops.cpp
@@ -173,6 +173,35 @@ int main(void)
 			}
 		}
 	}
+	
+	
+	/*
+	* Name: Fibonacci Series - first 25 term
+	* Requirement: the i-th item of the series equals to the sum of previous two
+	*	       print a new line every five numbers in console
+	*
+	* Author: Xuhua Huang
+	* Last updated: Nov 06, 2020
+	* Created on: Nov 06, 2020
+	*/
+	int current; // currentTerm = previous + SecondPrevious
+	int previous;
+	int secondPrevious;
+
+	current = previous = secondPrevious = 1;
+	cout << previous << " " << current << " ";
+
+	for (int i = 3; i <= 25; i++)
+	{
+		current = previous + secondPrevious;
+		cout << current << " ";
+
+		if (i % 5 == 0)
+			cout << endl;
+
+		secondPrevious = previous; // refresh the value in the series
+		previous = current;
+	}
 
 	return 0;
 }
