8c7ed12efb3867e7969fdb9eace07f8bcd43610c
Xuhua Andy Huang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-06 19:21:42 -0500

    Added Taylor series and right angle triangle with customized character.

--- Fun/fun_math_with_loops.cpp
+++ Fun/fun_math_with_loops.cpp
@@ -60,8 +60,55 @@ int main(void)
 	} while (userInput);
 	cout << "The number " << tempStorage << " is a number of " << numDigits << " digit(s). " << endl;
 
-	// adding n fractions numbers together
+	/*
+	* Taylor Series
+	* Analysis: adding n fractions numbers together
+	* numFraction: userinput, stands for number of fraction being added together
+	* sign: determine the sign of each fraction in this series
+	*/
+	int numFraction = 0;
+	cout << "Generating a Taylor Seiries of customized number of fractions" << endl
+		 << "Please enter the number of the fraction numbers in the taylor series: ";
 
+	cin >> numFraction;	 // index of denominator
+	
+	// creating required variables
+	double result = 0.0;
+	double  term = 0.0; // i_th term = sign / index
+	double sign = 1.0;
+
+	for (int i = 1; i <= numFraction; i++)
+	{
+		term = sign / numFraction;
+		result += term;
+		sign *= -1;
+	}
+	cout << "With " << numFraction << " number of fractions, the result of the series is: " << result << endl;
+
+	// output all the even number within the range of 1-100
+	for (int i = 1; i <= 100; i++)
+	{
+		if ( i % 2 == 0 )
+			cout << i << " is an even numer" << endl;
+		//else if (i % 2)
+			//cout << i << " is an odd number." << endl;
+	}
+
+	// creating a right-angle-triangle with "numCh" amount of "userCh"
+	// Hint: determine the relationship between number of characters and the line index of nested for loops
+	char userCh; // custom character to create right-angle triangle with
+	cin >> userCh;
+	int numCh; // number of character to form the side
+	cin >> numCh;
+
+	for (int i = 0; i < numCh; i++)
+	{
+		for (int j = 0; j < (i + 1); j++) // 'j< (i + 1);' is the key statement
+		{
+			cout << userCh;
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
