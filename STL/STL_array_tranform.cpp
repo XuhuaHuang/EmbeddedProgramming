2886113f5358a9aaa6e97ad4a56a37e95f1c5173
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-12 13:50:07 -0500

    Work with integer array using tranform function.
    std::transform with iterator

---
+++ STL/STL_array_tranform.cpp
@@ -0,0 +1,25 @@
+// A C++ program uses transform() in STL to add  
+// 1 to all elements of arr[]
+
+// std::transform(Iterator inputBegin, Iterator inputEnd, Iterator OutputBegin, function_Name)
+
+#include <bits/stdc++.h> 
+using namespace std; 
+   
+int increment(int x) {  return (x+1); } 
+   
+int main() 
+{ 
+    int arr[] = {1, 2, 3, 4, 5}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+   
+    // Apply increment to all elements of 
+    // arr[] and store the modified elements 
+    // back in arr[] 
+    transform(arr, arr+n, arr, increment); 
+   
+    for (int i=0; i<n; i++) 
+        cout << arr[i] <<" "; 
+   
+    return 0; 
+} 
