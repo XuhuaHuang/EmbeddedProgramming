236f79540bce6cac597d9cf59c722007693e392a
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-11 19:23:34 -0500

    Contains main function to test class 'mydate', 'mytime' and 'Calendar'.

---
+++ Pointer/Pointer_notes_main.cpp
@@ -0,0 +1,20 @@
+/*
+* Author: Xuhua Huang
+* Taught by: Charmaine
+* Pointer Lab Main Function
+*
+* Contains main function working with class 'Calendar'
+*/
+
+#include <iostream>
+#include "Pointer_notes.h"
+
+using namespace std;
+
+int main(void)
+{
+	Calendar myCalendar;
+	myCalendar.print();
+
+	return 0;
+}
