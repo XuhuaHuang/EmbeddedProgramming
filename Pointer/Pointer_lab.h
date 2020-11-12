5559719d8abf077831369ff1f8e705e6a03b6cfb
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-11 19:25:17 -0500

    Header for class 'Student', 'StudentClub' and 'Numbers'.

---
+++ Pointer/Pointer_lab.h
@@ -0,0 +1,90 @@
+/*
+* Author: Xuhua Huang
+* Teacher: Charmaine
+* Pointer Lab Header File
+*
+* Contains declaration of:
+* class 'Student'
+* class
+* class
+*	- constructor and destructor
+*
+* Notice: ALL "cout" statements inside a function are appened
+* after signature "[fn]" which stands for printing from function
+* [DECONST] printed from default constructor
+* [OLCONST] printed from overloaded constructor
+* [DEST] printed from destructor
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+public:
+
+	Student(); // default constructor
+	Student(string student_name, string major_at_acceptance); // overloaded constructor
+
+	// set and get functions
+	void setMajor(string new_major) { major = new_major; }
+	void setName(string new_name) { name = new_name; }
+	string getMajor() { return major; }
+	string getName() { return name; }
+
+private:
+	string name;
+	string major;;
+};
+
+class StudentClub
+{
+public:
+
+	// desturctor
+	~StudentClub();
+
+	// set and get functions:
+	void setPresident(Student* newPresident) { President = newPresident; }
+	Student* getPresident() { return President; }
+
+	void setVicePresident(Student* newVicePresident) { VicePresident = newVicePresident; }
+	Student* getVicePresident() { return VicePresident; }
+
+	void setSecretary(Student* newSecretary) { Secretary = newSecretary; }
+	Student* getSecretary() { return Secretary; }
+
+	void setTreasurer(Student* newTreasurer) { Treasurer = newTreasurer; }
+	Student* getTreasurer() { return Treasurer; }
+
+private:
+
+	Student* President, * VicePresident, * Secretary, * Treasurer;
+};
+
+class Numbers
+{
+public:
+
+	// set and get functions:
+	void setSize(int newSize) { size = newSize; }
+	int getSize() { return size; }
+
+	void setPtrNum(double* newPtrNum) { ptrNumberArray = newPtrNum; }
+	double* getPtrNum() { return ptrNumberArray; }
+
+	// overloaded constructor and destructor:
+	Numbers(int sizeOfArray); // overloaded constructor
+	~Numbers();
+
+	void store(int arrayIndex, double numToAdd);
+	double retreive(int arrayIndex);
+	double average();
+
+private:
+
+	int size;
+	double* ptrNumberArray; // X.H. renamed variable poiter 'ptrNumber' to 'ptrNumberArray'
+};
