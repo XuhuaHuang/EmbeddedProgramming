2530adf36d1e5e7f1a02cf1666f34dd3078553d1
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-12 15:12:54 -0500

    Compare two integers values with pointer. Dual results with one function of return type "void".

---
+++ Pointer/Pointer_compare.cpp
@@ -0,0 +1,38 @@
+// Modified program using References in C++
+#include <iostream>
+
+using namespace std;
+
+void compare(int a, int b, int& addr_greater, int& addr_smaller)
+{
+	if (a > b)
+	{
+		addr_greater = a;
+		addr_smaller = b;
+	}
+	else
+	{
+		addr_greater = b;
+		addr_smaller = a;
+	}
+}
+
+// Driver code 
+int main(void)
+{
+	int greaterNum, smallerNum, num1, num2;
+
+	cout << "Enter the first number of two to compare: ";
+	cin >> num1;
+	cout << "Enter the second number of two to compare: ";
+	cin >> num2;
+
+	// The last two arguments are passed
+	// by giving addresses of memory locations
+	compare(num1, num2, greaterNum, smallerNum);
+	
+	cout << "\nThe greater number is: " << greaterNum << endl
+		 << "The smaller number is: " << smallerNum << endl;
+
+	return 0;
+}
