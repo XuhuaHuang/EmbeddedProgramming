0832f5aec3e9bd079da79b3b4e4b52694fefb5ba
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-16 14:59:54 -0500

    class bat

---
+++ SFML/PongGame/bat.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+ 
+using namespace sf;
+ 
+class Bat
+{
+private:
+    Vector2f position;
+ 
+    // A RectangleShape object
+    RectangleShape batShape;
+ 
+    float batSpeed = .3f;
+ 
+public:
+    Bat(float startX, float startY);
+ 
+    FloatRect getPosition();
+ 
+    RectangleShape getShape();
+ 
+    void moveLeft();
+ 
+    void moveRight();
+ 
+    void update();
+ 
+};
