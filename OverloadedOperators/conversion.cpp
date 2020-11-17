f5b86288c0b9b8e584613330c0cf4d2e7bce48ae
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2020-11-17 14:47:50 -0500

    Overloaded conversion operator float()

---
+++ OverloadedOperators/conversion.cpp
@@ -0,0 +1,37 @@
+/*****************************************************************//**
+ * \file   conversion.cpp
+ * \brief  Overloaded conversion operator float()
+ * 
+ * Document highlight: n/a
+ * 
+ * \author 1879700
+ * \date   November 2020
+ *********************************************************************/
+
+#include <iostream> 
+using namespace std;
+
+class Fraction
+{
+private:
+    int num, den;
+
+public:
+    Fraction(int n, int d) { num = n; den = d; }
+
+    // conversion operator: return float value of fraction 
+    operator float() const
+    {
+        return float(num) / float(den);
+    }
+};
+
+int main(void)
+{
+    Fraction f(2, 5); // numerator = 2, denominator = 5
+    
+    float val = f; // float(val) is overloaded
+    cout << val;
+    
+    return 0;
+}
