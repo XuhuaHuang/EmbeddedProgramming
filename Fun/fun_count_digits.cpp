48af188eab48cd57bfd9f589c2b60385a64dc172
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2021-02-25 12:41:28 -0500

    contains function definition and calling to count the number of digits in an integer

---
+++ Fun/fun_count_digits.cpp
@@ -0,0 +1,31 @@
+/*****************************************************************//**
+ * \file   fun_count_digits.cpp
+ * \brief  contains function definition and calling
+ *		   to count the number of digits in an integer
+ * 
+ * \author Xuhua HUANG
+ * \date   February 2021
+ *********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+size_t numDigits(int number);
+
+int main(void)
+{
+	for (int i = 0; i <= 100; ++i) {
+		cout << "The number of digit of number " << i << " is: " << numDigits(i) << endl;
+	}
+
+	cout << "\nSingle calling.\nThe number of digit of number 20 is: " << numDigits(20) << endl;
+	return 0;
+}
+
+size_t numDigits(int number)
+{
+	std::size_t digitsCount = 1;
+	while ((number /= 10) != 0) { ++digitsCount; }
+
+	return digitsCount;
+}
