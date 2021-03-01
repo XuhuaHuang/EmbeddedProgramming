a589096ed7daf9e4cda4f7db4acc60f8b3c0a4d6
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2021-02-28 19:33:11 -0500

    pure virtual function in C++: can not instantiate instances without implementing  pure virtual function in the class

---
+++ Fun/fun_interface.cpp
@@ -0,0 +1,68 @@
+/*****************************************************************//**
+ * \file   fun_interface.cpp
+ * \brief  pure virtual function in C++
+ *		   can not instantiate instances without implementing
+ *		   pure virtual function in the class
+ *
+ * \author Xhuhua Huang
+ * \date   February 2021
+ *********************************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// class Printable only contains a pure virtual function that
+// acts like an interface keyword in other programming languages
+class Printable
+{
+public:
+	// create an interface that every derived class has to implement
+	// pure virtual function
+	virtual string getClassName() = 0;
+};
+
+// function prototype
+void printClassName(Printable* obj);
+
+class Entity : public Printable
+{
+private:
+	string className;
+public:
+	// implement defined interface
+	string getClassName() override { return "Entity : public Printable"; }
+};
+
+class User : public Entity
+{
+private:
+	string firstName;
+	string lastName;
+public:
+	// implement defined interface
+	string getClassName() override { return "User : Entity"; }
+
+	// constructor and get function
+	User(string fn, string ln) : firstName(fn), lastName(ln) {}
+	string getUserName() { return firstName + lastName; }
+};
+
+void printClassName(Printable* obj)
+{
+	cout << "Print class name: " << obj->getClassName() << endl;
+}
+
+int main(void)
+{
+	Entity* e = new Entity();
+	printClassName(e);
+
+	User* u = new User("Xuhua", "Huang");
+	printClassName(u);
+
+	delete e;
+	delete u;
+	return 0;
+}
