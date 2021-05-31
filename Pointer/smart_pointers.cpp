3328cdb71d4ad232983f698890590ddad774e19d
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-05-31 12:01:08 -0400

    Create smart_pointers.cpp

---
+++ Pointer/smart_pointers.cpp
@@ -0,0 +1,46 @@
+/*****************************************************************//**
+ * \file   smart_pointers.cpp
+ * \brief  contains notes on smart pointers in C++
+ * 
+ * \author Xuhua Huang
+ * \date   May 2021
+ *********************************************************************/
+
+#include <memory>
+#include <iostream>
+
+class Entity
+{
+public:
+	Entity() {
+		std::cout << "[DFCONST]Creating Entity..." << std::endl;
+	}
+
+	~Entity() {
+		std::cout << "[DFDEST]Destroying Entity..." << std::endl;
+	}
+
+	void printEntity() {
+		std::cout << "[fn]Printing Entity..." << std::endl;
+	}
+};
+
+int main(void)
+{
+	/**
+	 * Create a new scope and verify the output in the terminal.
+	 * Expecting constructor, destructor get called sequencially.
+	 * \return 
+	 */
+	{ 
+		std::unique_ptr<Entity> entity_ptr(new Entity()); // not exception safe 
+		// better approach: make_unique<Entity>
+		// equivalent to: std::unique_ptr<Entity> entity_ptr = std::make_unique<Entity>();
+		entity_ptr->printEntity(); // using pointer as how normally it is used
+	} // destructor gets called automatically when pointer goes out of scope
+
+	// share_ptr uses reference count approach to determine when to delete the pointer
+	std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
+	std::weak_ptr<Entity> weakEntity = sharedEntity; // does not increase refernce count
+	std::cin.get();
+}
