7341e9c18facf021019a45761f671d2e331b7aaf
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-06-15 13:33:28 -0400

    Uploaded an example on looking for prime numbers.

---
+++ Fun/Python/Cython/3_prime_number/primes_cpp.pyx
@@ -0,0 +1,21 @@
+from libcpp.vector cimport vector
+
+# distutils: language = c++
+def primes_cpp(unsigned int nb_primes):
+    cdef int n, i
+    cdef vector[int] p
+    p.reserve(nb_primes)  # allocate memory for 'nb_primes' elements
+
+    n = 2
+    while p.size() < nb_primes:  # size() for vectors is similar to len()
+        for i in p:
+            if n % i == 0:
+                break
+        else:
+            p.push_back(n)  # push_back is similar to append()
+        n += 1
+
+    # vectors are automatically converted to Python
+    # lists when converted to Python objects
+    print(p)
+    return p
