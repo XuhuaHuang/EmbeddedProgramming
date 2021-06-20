b76703a7a6ea990c6a07bfd43d2ad2eb8708d1a9
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-06-19 21:40:16 -0400

    Uploaded notes on profiling Cython with cProfile.

---
+++ Fun/Python/Cython/9_profiling/profiler.py
@@ -0,0 +1,14 @@
+import pstats, cProfile
+
+'''
+Added for file 'calc_pi.pyx' 
+'''
+import pyximport
+pyximport.install()
+
+import calc_pi
+
+cProfile.runctx("calc_pi.approx_pi()", globals(), locals(), "Profile.prof")
+
+s = pstats.Stats("Profile.prof")
+s.strip_dirs().sort_stats("time").print_stats()
