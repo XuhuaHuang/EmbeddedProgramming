b821201b5e12568eb9ca1472c3c44165817a4afd
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-08-25 16:52:18 -0400

    Daily maintenance task.

--- FriendClass/example_Square_Rectangle.cpp
+++ FriendClass/example_Square_Rectangle.cpp
@@ -1,15 +1,15 @@
 /*****************************************************************//**
  * \file   example_Square_Rectangle.cpp
  * \brief  class "Square" made friend of class "rectangle"
- * 
+ *
  * worth noting:
  * 1) granting the other class to access the protected and private
  *	  data members of the class made friend with.
  * 2) class friendship is not bidirectional. Need to specify.
- * 3) class friendship is not transmitive:
- *		The friend of a friend is not considered to be a friend
- *		unless explicitly specified.
- * 
+ * 3) class friendship is not transmissive:
+ *	  The friend of a friend is not considered to be a friend
+ *	  unless explicitly specified.
+ *
  * \author Xuhua
  * \date   November 2020
  *********************************************************************/
@@ -23,12 +23,12 @@ private:
 	int side;
 
 public:
-	
 	void setSide(int a) { side = a; }
 
 	// Square class allows Rectangle class to access its private data members.
 	friend class Rectangle;
 	// conflict the concept of class encapsulation
+	// could be avoided by using public get functions (getter)
 };
 
 class Rectangle
@@ -37,12 +37,11 @@ private:
 	int width, height;
 
 public:
-
 	int area() { return (width * height); }
-	void convert(Square a);
+	void convert(const Square&);
 };
 
-void Rectangle::convert(Square a)
+void Rectangle::convert(const Square& a)
 {
 	width = a.side;
 	height = a.side;
@@ -57,7 +56,7 @@ int main(void)
 	Rectangle rect;
 
 	sqr.setSide(4);
-	rect.convert(sqr);
+	rect.convert(sqr);  // converting a square to rectangle
 
 	cout << rect.area() << endl;
 
