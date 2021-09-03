b28a29dd3bbefba1dafcb937d661a2c5d1ea9621
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-09-02 21:24:41 -0400

    Create Person.h

---
+++ InterfacePimpl/InterfacePimpl/Person.h
@@ -0,0 +1,59 @@
+/*****************************************************************//**
+ * \file   Person.h
+ * \brief  Contains pure virtual functions for an interface `Person`
+ *
+ * \author Xuhua Huang
+ * \date   September 2021
+ *********************************************************************/
+
+#pragma once
+
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <iostream>
+#include <string>
+
+ /* Use forward declaration to provide class interfaces. */
+ /* Do NOT need to recompile unless the class name changes. */
+class Date;
+class Address;
+
+/* Person interface. */
+class Person
+{
+public:
+	virtual ~Person();
+
+	/**
+	 * It is impossible to instantiate an instance for this interface;
+	 * however, its child classes have to have access to a so-called `factory` function.
+	 */
+	virtual std::string name() const = 0;
+	virtual std::string birthDate() const = 0;
+	virtual std::string address() const = 0;
+
+	/* Factory function to be used by child classes. */
+	/* Declared static for syntax `Person::createPerson()`. */
+	static std::tr1::shared_ptr<Person> createPerson(
+		const std::string& name,
+		const Date& birthday,
+		const Address& addr
+	);
+};
+
+/** REMEMBER: derive class write factory functions
+ *
+ * Use case for the factory/virtual constructor for interface Person:
+ * std::string name;
+ * Date dateOfBirth;
+ * Address liveAddr;
+ *
+ * // creating an object that implements the `Person` interface
+ * std::tr1::shared_ptr<Person> ptrPerson(Person::createPerson(name, dateOfBirth, liveAddr));
+ * // accessing info of such person using const getters
+ * std::cout << ptrPerson->name()
+ *			 << ptrPerson->birthDate()
+ *			 << ptrPerson->address();
+ */
+#endif
