3daa6fde3c08920e52773c95a65c3b5bef642ed0
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-09-29 22:03:33 -0400

    Added notes and performed code reviews.

--- Vector/vectorNotes.cpp
+++ Vector/vectorNotes.cpp
@@ -1,15 +1,49 @@
+/*****************************************************************//**
+ * \file   vectorNotes.cpp
+ * \brief  When the vector is full, it creates a new one with 50% more
+ * space and move(or copy) the original data.
+ * Note that the memory is allocated in a contiguous manner.
+ * 
+ * Despite the fact objects copy mechanism when stored in vector,
+ * remember to provide a sanitized copy constructor.
+ * 
+ * Less commonly-seen member function of <vector>:
+ * 1) .reserve(capacity)
+ * 2) .emplace_back(elem) is a better approach to avoid copying
+ * 
+ * \author Xuhua Huang
+ * \date   February 2021
+ *********************************************************************/
+
 #include <iostream>
+#include <stdlib.h>
 #include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+namespace Util {
+	namespace vector {
+
+		/* Template to print a std::vector with O(n). */
+		template<typename T>
+		void print_vec(const std::vector<T>& Vec) {
+			/* Loop through the vector with range-based for loop. */
+			for (const T& t : Vec)
+				std::cout << t << std::endl;
+
+			return; // add empty return statement for consistency.
+		}
+	}
+}
+
 void printVector(vector<double> dv);
 void func(double value);
 
 int main(void)
 {
+	/* Using template constructor */
 	vector<int> v1(6,0); // create an array of 6 elements and initialize them to zero
 	cout << v1.at(0) << endl; // display zero from position indexed 0
 	
@@ -65,25 +99,33 @@ int main(void)
 	for_each(dv.begin(),dv.end(),func);
 	
 	// counts the number of times the element shows up
-	int c = count(dv.begin(), dv.end(),9.9);
+	int c = count(dv.begin(), dv.end(), 9.9);
 	cout << c << endl;
 	
+	system("pause");
 	return 0;
 }
 
 
 void printVector(vector<double> dv)
 {
+	/* Constant references could not be used here
+	 * due the nature of an iterator.
+	 */
 	vector<double>::iterator iterdouble;
 	for(iterdouble = dv.begin();iterdouble < dv.end(); iterdouble++)
 	{
 		cout << *iterdouble << endl;
 	}
+
+	return;
 }
 
 
 void func(double value)
 {
 	cout << "Inside  function func(): ";
-	cout << value << endl;	
+	cout << value << endl;
+
+	return;
 }
