4a85ad195f8c19db4fad3cecdac4c6f7842fa265
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-10-13 21:45:17 -0400

    Create demo_parse_str.cpp

---
+++ demo_parse_str.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************//**
+ * \file   demo_parse_str.cpp
+ * \brief  Demonstration of function to parse a comma-separated string
+ * into integers and store them in a std::vector<int>.
+ *
+ * \author Xuhua Huang
+ * \date   October 2021
+ *********************************************************************/
+
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <sstream>
+#include "Util/parse_str.h"
+
+int main() {
+	std::string str;
+	//std::cin >> str;
+	str = "1,23,456,789";
+	std::vector<int> integers = Util::parse::parse_str<int>(str);
+	for (int i = 0; i < integers.size(); i++) {
+		std::cout << integers[i] << "\n";
+	}
+
+	system("pause");
+	return 0;
+}
