0a306c79c61503b9e56e31e53abc8e49df49789c
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-10-16 21:35:27 -0400

    Add demonstration with custom delimiter.

--- demo_parse_str.cpp
+++ demo_parse_str.cpp
@@ -14,12 +14,22 @@
 #include "Util/parse.h"
 
 int main() {
+
+	/* Split a comma-separated string to an array of integers. */
 	std::string str;
 	// std::cin >> str;
-	str = "1,23,456,789";
-	std::vector<int> integers = util::parse::parse_str<int>(str);
+	str = "1,23,456,789";						// demo with a comma as delimiter
+	std::string cell_phone = "541-952-1655";	// demo with a dash as delimiter
+	std::vector<int> integers = util::parse::parse_str<int>(cell_phone, '-');
 	for (int i = 0; i < integers.size(); i++) {
 		std::cout << integers[i] << "\n";
+	}
+
+	/* Split a comma-separated string to an array of characters. */
+	const std::string name = "Huang,Xuhua";
+	std::vector<char> spell_name = util::parse::parse_str<char>(name);
+	for (int j = 0; j < spell_name.size(); ++j) {
+		std::cout << spell_name[j] << "\n";
 	}
 
 	system("pause");
