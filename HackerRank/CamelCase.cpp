7679ec2125159922b093ce041a15de407691807d
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-10-17 16:39:50 -0400

    Create CamelCase.cpp

---
+++ HackerRank/CamelCase.cpp
@@ -0,0 +1,47 @@
+/*****************************************************************//**
+ * \file   CamelCase.cpp
+ * \brief  Test question on HackerRank platform.
+ * https://www.hackerrank.com/challenges/camelcase/problem
+ * Count the total number of words in a camelCase formatted string.
+ * Passes all test cases and the code is submitted.
+ * 
+ * Command used to compile:
+ * $ g++ -o camelCase.exe .\CamelCase.cpp -std=c++11
+ * $ .\camelCase.exe
+ * 
+ * \author Xuhua Huang
+ * \date   October 2021
+ *********************************************************************/
+
+#include <iostream>
+#include <string>
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+int camelcase(const std::string s) {
+    int word_count = 1;
+
+    for (int i = 0; i < s.size(); ++i) {
+        if (isupper(s.at(i))) { // equivalent to: isupper(s[i])
+            word_count++;
+        }
+    }
+
+    return word_count;
+}
+
+int main()
+{
+    std::string s;
+    std::getline(std::cin, s); // get a camelCase formatted string from user
+
+    int result = camelcase(s);
+
+    std::cout << result << "\n";
+
+    return 0;
+}
