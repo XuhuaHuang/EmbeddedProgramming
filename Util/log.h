4c432437aebe5c308ef149edd7f779e55d69a9b6
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-10-28 16:25:13 -0400

    Create log.h

---
+++ Util/log.h
@@ -0,0 +1,22 @@
+/*****************************************************************//**
+ * \file   log.h
+ * \brief  Custom LOG() macro to print location of the function call
+ * with __VA_ARGS__ and fprintf() in C.
+ * 
+ * \author Xuhua Huang
+ * \date   October 2021
+ *********************************************************************/
+
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+
+/* Global Macro Definition */
+#define LOG(...) {\
+    fprintf(stderr, "%s: Line %d:\t", __FILE__, __LINE__);\
+    fprintf(stderr, __VA_ARGS__);\
+    fprintf(stderr, "\n");\
+}
+
+#endif
