442c24c734640506dc071136f60026a160fe086b
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-10-28 16:26:44 -0400

    Create print_list.h

---
+++ Util/print_list.h
@@ -0,0 +1,42 @@
+/*****************************************************************//**
+ * \file   print_list.h
+ * \brief  Template function to print std::list of any types.
+ *
+ * \author Xuhua Huang
+ * \date   October 2021
+ *********************************************************************/
+
+#ifndef PRINT_LIST_H
+#define PRINT_LIST_H
+
+#include <iostream>
+#include <list>
+
+#ifndef DEBUG
+#define DEBUG(arg_str) std::cout << arg_str << std::endl;
+#endif
+
+namespace util {
+	namespace list {
+
+        /* Template to print a std::list with O(n). */
+        /* For sanity, using constant reference instead of iterators. */
+        template<typename T>
+        static void print_list(std::list<T> arg_list)
+        {
+            DEBUG("\n[fn]Util::list::print_list");
+
+            /* Loop through the vector with iterator. */
+            typename std::list<T>::iterator iter; // keyword "typename" is required for iterator
+            for (iter = arg_list.begin(); iter != arg_list.end(); ++iter)
+                std::cout << *iter << " "; // dereference the iterator to print content
+
+            DEBUG("[fn]Finished printing the list.");
+            return;
+        }
+
+	} // end Util::list
+} // end Util
+
+
+#endif
