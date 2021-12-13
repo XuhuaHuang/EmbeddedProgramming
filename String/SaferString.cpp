e2d9f43f6a18bdf1d86b9ea3ec061fadbf0d53f6
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-12-13 18:22:05 -0500

    Update SaferString.cpp

---
+++ String/SaferString.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file SaferString.cpp
+ * @author Xuhua Huang (xuhuahuang0412@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+int main(void) {
+
+    system("pause");
+    return 0;
+}
