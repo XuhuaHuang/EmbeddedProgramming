4125214ec4e528a6eeaf786d41da4cdd8b8b3ecb
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-01-06 22:02:25 -0500

    Uploaded draft.

---
+++ Lambda/lambda_with_stdlib.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file lambda_with_stdlib.cpp
+ * @author Xuhua Huang (xuhuahuang0412@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+
+int main(void) {
+
+    system("pause");
+    return 0;
+}
