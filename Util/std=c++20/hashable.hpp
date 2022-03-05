4b0add102031f27ec204ed9d3fabd7b9c0a1cd1d
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-03-05 10:46:22 -0500

    demonstrate cpp20 concept usage.

---
+++ Util/std=c++20/hashable.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file hashable.hpp
+ * @author Xuhua Huang (huang.xuhua@outlook.com)
+ * @brief C++20 feature concepts to implement a template
+ * to determine whether an object is 'hashable'.
+ * @version 0.1
+ * @date 2022-01-27
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <concepts> // require C++20 standard
+
+namespace util {
+    namespace type_safety {
+        
+        /* Templated concept to determine whether can call hash function on an object. */
+        template<typename T>
+        concept hashable = requires(T t) {
+            { std::hash<T>{}(t) } -> std::convertible_to<std::size_t>;
+        }
+    
+    } // end type_safety
+} // end util
