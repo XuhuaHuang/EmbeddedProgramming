707d65923fa15edc1d3f229e838e0702a02605ee
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-03-05 10:41:18 -0500

    Reformatted the file and add comments.

--- HashTable/main.c
+++ HashTable/main.c
@@ -3,12 +3,13 @@
  * @author Xuhua Huang (xuhuahuang0412@gmail.com)
  * @brief Understanding and implementing a hash table in C.
  * Constant operation time of O(1) with Open Addressing and External Chamber.
+ * 
  * To run the file on Windows with MinGW:
- * $ mingw32-make
+ * $ mingw32-make all
  * $./hashtable
  * 
  * @version 0.1
- * @date 2021-11-03
+ * @date 2022-02-03
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -20,16 +21,24 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#ifndef MAX_NAME
 #define MAX_NAME 256
+#endif
+
+#ifndef TABLE_SIZE
 #define TABLE_SIZE 10
+#endif
+
+#ifndef DELETED_NODE
 #define DELETED_NODE (CPerson*)(0xFFFFFFFFFFFFFFFFUL)
+#endif
 /* UL at the end of the hex address for DELETED_NODE means 'unsigned long'. */
 
 /**
  * @brief Define a C-style struct
  * and name it `CPerson` 
  */
-typedef struct {
+typedef struct CPerson {
     char name[MAX_NAME];
     int age;
 } CPerson;
@@ -38,7 +47,7 @@ typedef struct {
  * @brief Create hash function.
  * @return unsigned int 
  */
-unsigned int hash(const char* const name) {
+unsigned int hash(const char* const name) {                 /* constant pointer object and constant content */
     int length = strnlen(name, MAX_NAME);
     unsigned int hash_value = 0;                            /* variable to store the sum of ASCII codes */
     for(int i = 0; i < length; ++i) {
@@ -49,6 +58,10 @@ unsigned int hash(const char* const name) {
     return hash_value;
 }
 
+/**
+ * @brief Define an C-style array
+ * to store pointer objects pointing to CPerson(s) objects
+ */
 CPerson* hash_table[TABLE_SIZE];
 
 /**
@@ -56,16 +69,17 @@ CPerson* hash_table[TABLE_SIZE];
  * @return void
  */
 void init_hash_table() {
-    /* global array of CPerson* is visiable in this function. */
+    /* global array of CPerson* is visible in this function. Defined at line 65. */
     for (int i = 0; i < TABLE_SIZE; ++i) {
         hash_table[i] = NULL;
     }
-    /* table is empty. */
+    /* array is initialized to null pointers. */
     return;
 }
 
 /**
  * @brief Define a function to print elements in the hash table.
+ * Hash table is global, therefore no parameter(s) is parsed.
  * @return void
  */
 void print_table() {
@@ -77,7 +91,7 @@ void print_table() {
             printf("\t%i\t---<deleted>\n", i);
 
         }
-        else{
+        else {
             printf("\t%i\t%s\n", i, hash_table[i]->name);
         }
     }
@@ -88,10 +102,10 @@ void print_table() {
 /**
  * @brief Define a function to insert a person in the table.
  * 
- * @param ptr 
+ * @param ptr (constant address, constant content)
  * @return bool -> either the operation was successful or not
  */
-bool insert_to_table(CPerson* ptr) {
+bool insert_to_table(const CPerson* const ptr) {
     /* verify pointer is not NULL. */
     if (ptr == NULL) {
         return false;
@@ -112,7 +126,7 @@ bool insert_to_table(CPerson* ptr) {
 /**
  * @brief Define a function to look up a person by name in the table.
  * 
- * @param name 
+ * @param name (constant address, constant content)
  * @return CPerson* 
  */
 CPerson* hash_table_lookup(const char* const name) {
@@ -149,23 +163,24 @@ CPerson* del_from_table(const char* const name) {
             continue;
         }
         if(strncmp(hash_table[try_to_locate]->name, name, TABLE_SIZE) == 0) {
-            
-            CPerson* temp = hash_table[try_to_locate];
-            hash_table[try_to_locate] = DELETED_NODE;
-            return temp;
+            CPerson* temp = hash_table[try_to_locate];      /* obtain a temporary copy to return */
+            hash_table[try_to_locate] = DELETED_NODE;       /* mark the returned hash table element as deleted */
+            return temp;                                    /* compiler optimization */
         }
     }
     return NULL;
 }
 
 int main(void) {
+
+    /* create and initialize a hash table. */
     init_hash_table();
     print_table();  /* expecting empty hash table. */
     
-    /* create multiple Cperson object with list initialization. */
-    CPerson jacob = {.name="Jacob", .age=40};
-    CPerson andy = {.name="Andy", .age=20};
-    CPerson liam = {.name="Liam", .age=34};
+    /* create multiple CPerson object with list initialization. */
+    CPerson jacob = { .name="Jacob", .age=40 };
+    CPerson andy = { .name="Andy", .age=20 };
+    CPerson liam = { .name="Liam", .age=34 };
 
     /* insert people to the hash table. */
     insert_to_table(&jacob);
