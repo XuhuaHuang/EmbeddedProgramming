96288fbdfa937e8598d1ebb8b4f3399878d3be4b
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-03-20 11:58:07 -0400

    Refactored.

--- Inheritance/Polymorphism/example_Polygon.cpp
+++ Inheritance/Polymorphism/example_Polygon.cpp
@@ -1,20 +1,19 @@
 /*****************************************************************//**
- * \file   example_Polymorphism.cpp
+ * \file   example_Polygon.cpp
  * \brief
- *   
+ *
  * 1) The virtual keyword allows a member function of a derived class 
  *	  with the same name as a member function in the base class to be 
  *	  appropriately called from a pointer.
  * 
  * 2) The type of the pointer is a pointer to the base class 
  *	  but is pointing to an object of the derived class
- * 
+ *
  * \author Xuhua
  * \date   November 2020
  *********************************************************************/
 
 #include <iostream>
-using namespace std;
 
 class Polygon
 {
@@ -23,44 +22,44 @@ protected:
 
 public:
 
-	// function prototype
-	void set_values(int a, int b) { width = a; height = b; }
+    // function prototype
+    void set_values(int a, int b) { width = a; height = b; }
 
-	// "Polygon" is a 'polymorphic class'
-	virtual int area() { return 0; } // virtual keyword ensures 
-	// Polygon* to access class function for address-linked-object 
+    // "Polygon" is a 'polymorphic class'
+    virtual int area() const { return 0; } // virtual keyword ensures
+    // Polygon* to access class function for address-linked-object
 };
 
 class Rectangle : public Polygon
 {
 public:
-	int area() { return (width * height); }
+    int area() const override { return (width * height); }
 };
 
 class Triangle : public Polygon
 {
 public:
-	int area() { return (width * height / 2); }
+    int area() const override { return (width * height / 2); }
 };
 
 int main()
 {
-	Rectangle rect;
-	Triangle trgl;
-	Polygon poly;
-	Polygon* ppoly1;
+    Rectangle rect;
+    Triangle trgl;
+    Polygon poly;
+    Polygon* ppoly1;
 
-	ppoly1 = &rect; // point to a "Rectangle" class object
-	ppoly1->set_values(4, 5);
-	cout << ppoly1->area() << endl; // will print 0 if without "virtual" keyword
+    ppoly1 = &rect; // point to a "Rectangle" class object
+    ppoly1->set_values(4, 5);
+    std::cout << ppoly1->area() << "\n"; // will print 0 if without "virtual" keyword
 
-	ppoly1 = &trgl; // pointing to a "Triangle" class object
-	ppoly1->set_values(4, 5);
-	cout << ppoly1->area() << endl; // will print 0 if without "virtual" keyword
+    ppoly1 = &trgl; // pointing to a "Triangle" class object
+    ppoly1->set_values(4, 5);
+    std::cout << ppoly1->area() << "\n"; // will print 0 if without "virtual" keyword
 
-	ppoly1 = &poly; // pointing to a same-base class obeject
-	ppoly1->set_values(4, 5);
-	cout << ppoly1->area() << endl; // 0
+    ppoly1 = &poly; // pointing to a same-base class object
+    ppoly1->set_values(4, 5);
+    std::cout << ppoly1->area() << "\n"; // 0
 
-	return 0;
+    return 0;
 }
--- Inheritance/Polymorphism/lab_Pet.cpp
+++ Inheritance/Polymorphism/lab_Pet.cpp
@@ -1,8 +1,11 @@
 /*****************************************************************//**
  * \file   lab_Polymorphism.cpp
  * \brief  lab on polymorphism with keyword "virtual"
- * \ "virtual" grants the pointer access to polymorphism member function
- * 
+ * "virtual" grants the pointer access to polymorphism member function
+ *
+ * $ g++ .\lab_Pet.cpp -o pet
+ * $ ./pet
+ *
  * \author Xuhua
  * \date   November 2020
  *********************************************************************/
@@ -10,111 +13,95 @@
 #include <iostream>
 #include <string>
 
-// all overloaded constructor are removed to 
-// avoid conflicting with virtual setAge function
-
-using namespace std;
-
 class Pet
 {
 protected: // X.H. changed the keyword to 'protected'
-	string name;
-	int age;
+    std::string name;
+    int age;
 
 public:
-	// default and overloaded constructors
-	Pet();
+    // default and overloaded constructors
+	Pet() {
+        setAge(0);
+        setName("");
+    }
 
-	// set and get function:
-	void setName(string newname) { name = newname; }
-	string getName() { return name; }
+    // set and get function:
+    void setName(std::string newname) { name = newname; }
+    std::string getName() const{ return name; }
 
-	int getAge() { return age; }
+    int getAge() const { return age; }
 
-	virtual void setAge(int newage) { age = 0; }
-	// X.H. changed the definition of 'setAge()' to set the age to 0
-	// uncomment "virtual" if need to revoke polymorphism member functions of derived class
+    virtual void setAge(int newage) { age = 0; }
+    // X.H. changed the definition of 'setAge()' to set the age to 0
+    // uncomment "virtual" if need to revoke polymorphism member functions of derived class
 };
 
-class Dog : public Pet // inheritaing from class 'Pet'
+class Dog : public Pet // inheriting from class 'Pet'
 {
 private:
-	string breed;
+    std::string breed;
 
 public:
-	// dafault and overloaded constructor
-	Dog();
-	// Dog(string newbreed, int newage, string newname); // -> pass age and name
-	// to parent class ooverloaded constructor
-	// : Pet(newage, newname) { setbreed(newbreed); setAge(getAge() * 7); }
+    // default and overloaded constructor
+    Dog() {
+        setAge(getAge() * 7); // access to class Pet is granted
+    }
 
-	// set and get function for new element string breed:
-	void setBreed(string newbreed) { breed = newbreed; }
-	string getBreed() { return breed; }
+    // set and get function for new element std::string breed:
+    void setBreed(std::string newbreed) { breed = newbreed; }
+    std::string getBreed() const { return breed; }
 
-	// new setAge function
-	void setAge(int newage) { age = (newage * 7); }
+    // new setAge function
+    void setAge(int newage) override { age = (newage * 7); }
 };
 
-class Cat : public Pet // inheritaing from class 'Pet'
+class Cat : public Pet // inheriting from class 'Pet'
 {
 private:
-	int numLives;
+    int numLives;
 
 public:
-	// default constructor
-	Cat();
+    // default constructor
+    Cat() {
+        setnumLives(9);
+    }
 
-	// set and get functions for new element int numLives:
-	void setnumLives(int newNumLives) { numLives = newNumLives; }
-	int getnumLives(void) { return numLives; }
+    // set and get functions for new element int numLives:
+    void setnumLives(int newNumLives) { numLives = newNumLives; }
+    int getnumLives(void) const { return numLives; }
 
-	// new setAge function
-	void setAge(int newage) { age = newage; }
+    // new setAge function
+    void setAge(int newage) { age = newage; }
 };
 
 int main(void)
 {
-	Pet myPet; // created a 'Pet' object
-	Pet* ptrPet = &myPet; // create a pointer of class 'Pet'
-						 // and linked it to Pet 'myPet'
+    Pet myPet; // created a 'Pet' object
+    Pet* ptrPet = &myPet;
+    // create a pointer of class 'Pet'
+    // and linked it to Pet 'myPet'
 
-	Dog myDog;
-	Pet* ptrDog = &myDog; // parent class pointer pointing to derived class object
+    Dog myDog;
+    Pet* ptrDog = &myDog; // parent class pointer pointing to derived class object
 
-	Cat myCat;
-	Pet* ptrCat = &myCat; // parent class pointer pointing to derived class object
+    Cat myCat;
+    Pet* ptrCat = &myCat; // parent class pointer pointing to derived class object
 
-	// with three pointers, setting all ages to 2
-	cout << "\nSetting ages of \"myPet\", \"myDog\" and \"myCat\" to 2." << endl;
-	ptrPet->setAge(2);
-	ptrDog->setAge(2);
-	ptrCat->setAge(2);
+    // with three pointers, setting all ages to 2
+    std::cout << "\nSetting ages of \"myPet\", \"myDog\" and \"myCat\" to 2." << "\n";
+    ptrPet->setAge(2);
+    ptrDog->setAge(2);
+    ptrCat->setAge(2);
 
-	cout << "\nTest case with virtual keyword: " << endl
-		 << "Obtaining age for Pet object which should be 0: " << ptrPet->getAge() << endl
-		 << "Obtaining age for Dog object which shoulf be 14: " << ptrDog->getAge() << endl
-		 << "Obtaining age for Cat object which should be 2: " << ptrCat->getAge() << endl;
+    std::cout << "\nTest case with virtual keyword: " << "\n"
+        << "Obtaining age for Pet object which should be 0: " << ptrPet->getAge() << "\n"
+        << "Obtaining age for Dog object which should be 14: " << ptrDog->getAge() << "\n"
+        << "Obtaining age for Cat object which should be 2: " << ptrCat->getAge() << "\n";
 
-	cout << "\nComment out the \"virtual\" keyword to verify all ages are 0." << endl
-		 << "\tSince all pointers are forced to go through" << endl
-		 << "\tmember function setAge() in \"Pet\" class." << endl;
+    std::cout << "\nComment out the \"virtual\" keyword to verify all ages are 0." << "\n"
+        << "\tSince all pointers are forced to go through" << "\n"
+        << "\tmember function setAge() in \"Pet\" class." << "\n";
 
-	return 0;
-}
-
-Pet::Pet() // default constructor for the Pet class
-{
-	setAge(0);
-	setName("");
-}
-
-Dog::Dog() // default constructor for Dog class
-{
-	setAge(getAge() * 7); // access to class Pet is granted
-}
-
-Cat::Cat() // default constructor for Cat class
-{
-	setnumLives(9);
+    return 0;
 }
