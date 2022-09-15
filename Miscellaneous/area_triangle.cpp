3ccad24c7b693ffbe0d70f439c1587cacc5338ae
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-15 00:28:29 -0400

    Replaced tabs with spaces.

--- Miscellaneous/area_triangle.cpp
+++ Miscellaneous/area_triangle.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
-
 int main(void)
 {
-	double a = 0, b = 0, c = 0;
-	cout << "Please enter the length of the first side of the triangle: ";
-	cin >> a;
+    using namespace std;
+    double a = 0, b = 0, c = 0;
+    cout << "Please enter the length of the first side of the triangle: ";
+    cin >> a;
 
-	cout << "Please enter the length of the second side of the triangle: ";
-	cin >> b;
+    cout << "Please enter the length of the second side of the triangle: ";
+    cin >> b;
 
-	cout << "Please enter the third length of the side of the triangle: ";
-	cin >> c;
+    cout << "Please enter the third length of the side of the triangle: ";
+    cin >> c;
 
-	double s = (0.5) * (a + b + c);
-	double area = sqrt(s * (s - a) * (s - b) * (s - c));
+    double s = (0.5) * (a + b + c);
+    double area = sqrt(s * (s - a) * (s - b) * (s - c));
 
-	cout << "\nThe area of the triangle you just entered is: " << area << endl;
+    cout << "\nThe area of the triangle you just entered is: " << area << endl;
 
-	return 0;
+    return 0;
 }
