0ff2a0a2c1dd996721f22256f1a3f890f9b3a3e3
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-15 00:34:59 -0400

    Create main.cpp

---
+++ Util/std=c++20/BasicPlane/BasicPlane/main.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************//**
+ * \file   main.cpp
+ * \brief  
+ * 
+ * \author Xuhua Huang (xuhua.huang.io@outlook.com)
+ * \date   September 2022
+ *********************************************************************/
+
+//import std.core;
+#include <iostream>
+import BasicPlane.Figures;
+
+int main(void)
+{
+    std::ios::sync_with_stdio(false);
+
+    //Point ul{ 1, 8 };
+    //Point lr{ 11, 3 };
+    //Rectangle r {ul, lr};
+    Rectangle r{ {1, 8}, {11, 3} };
+
+    std::cout << "height: " << height(r) << "\n";
+    std::cout << "width: " << width(r) << "\n";
+    std::cout << "area: " << area(r) << "\n";
+
+    return 0;
+}
