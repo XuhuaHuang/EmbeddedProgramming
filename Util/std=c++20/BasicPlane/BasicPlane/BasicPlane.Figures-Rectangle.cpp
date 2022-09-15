c1ffb4a86374bac4a05285f6e1c293e5137c0b49
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-15 00:34:29 -0400

    Create BasicPlane.Figures-Rectangle.cpp

---
+++ Util/std=c++20/BasicPlane/BasicPlane/BasicPlane.Figures-Rectangle.cpp
@@ -0,0 +1,25 @@
+/*****************************************************************//**
+ * \file   BasicPlane.Figures-Rectangle.cpp
+ * \brief  
+ * 
+ * \author Xuhua Huang (xuhua.huang.io@outlook.com)
+ * \date   September 2022
+ *********************************************************************/
+
+module;
+
+// global module fragment area. Put #iclude directives here.
+
+module BasicPlane.Figures:Rectangle;
+
+int area(const Rectangle& rec) {
+    return height(rec) * width(rec);
+}
+
+int height(const Rectangle& rec) {
+    return rec.ul.y - rec.lr.y;
+}
+
+int width(const Rectangle& rec) {
+    return rec.lr.x - rec.ul.x;
+}
