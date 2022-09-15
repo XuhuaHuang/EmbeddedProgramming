9aa8399a6c8fe0174afa0c95c5a29f5ab6d49889
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-15 00:35:31 -0400

    Uploaded ModuleExample.

---
+++ Util/std=c++20/ModuleExample/ModuleExample/MyProgram.cpp
@@ -0,0 +1,18 @@
+/*****************************************************************//**
+ * \file   MyProgram.cpp
+ * \brief  Demonstration to import a C++20 module from a interface file
+ * 
+ * \author Xuhua Huang (xuhua.huang.io@outloook.com)
+ * \date   September 2022
+ *********************************************************************/
+
+// The import declaration can appear only at global scope
+import Example;
+//import std.core;
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "The result of fn() is " << Example_NS::fn() << std::endl; // 42
+    return 0;
+}
