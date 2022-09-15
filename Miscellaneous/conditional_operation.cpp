cdc021bbd020c9c1b6b4c4334bd83fe8b5c1bd4e
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-15 00:28:29 -0400

    Replaced tabs with spaces.

--- Miscellaneous/conditional_operation.cpp
+++ Miscellaneous/conditional_operation.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
-
 int main(void)
 {
-	cout << "\nConditional Operation" << endl;
-	int x = 1, y = 3;
-	int result = (y - x) > 0 ? y - x : x - y;
-	cout << result << endl;
-	
-	/*
-	if (y - x > 0)
-		result = y - x;
-	else
-		result = x - y;
-	*/
-  
-	// nested conditional operation
-	cout << "\nNested conditional operation" << endl;
-	int signX = determineSign(x);
-	cout << signX << endl;
-	
-	return 0;
+    using namespace std;
+
+    cout << "\nConditional Operation" << endl;
+    int x = 1, y = 3;
+    int result = (y - x) > 0 ? y - x : x - y;
+    cout << result << endl;
+
+    /*
+    if (y - x > 0)
+        result = y - x;
+    else
+        result = x - y;
+    */
+
+    // nested conditional operation
+    cout << "\nNested conditional operation" << endl;
+    int signX = determineSign(x);
+    cout << signX << endl;
+
+    return 0;
 }
 
 int determineSign(int num)
 {
-	return (num > 0 ? 1 : (n < 0 ? -1 : 1)); // return +1 if positive, -1 if negative, 0 if zero
+    return (num > 0 ? 1 : (n < 0 ? -1 : 1)); // return +1 if positive, -1 if negative, 0 if zero
 }
