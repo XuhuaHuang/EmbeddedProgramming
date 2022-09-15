846509701b2753c768b9e2f640e6539d4d769f34
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-14 23:26:06 -0400

    Added compile command and uploaded precompiled header.

--- Util/is_same_type.hpp
+++ Util/is_same_type.hpp
@@ -3,8 +3,19 @@
  * @author Xuhua Huang (xuhuahuang0412@gmail.com)
  * @brief 
  * @version 0.1
- * @date 2022-02-11
+ * @date 2022-09-14
+ * 
+ * $ g++ is_same_type.hpp -std=c++2a
  * 
  * @copyright Copyright (c) 2022
  * 
  */
+
+#if __has_include(<typeinfo>)
+#include <typeinfo>
+#endif
+
+template<typename T, typename U>
+constexpr inline bool is_same_type(const T& t, const U& u) {
+    return typeid(t).hash_code() == typeid(u).hash_code();
+}
