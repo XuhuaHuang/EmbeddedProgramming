fc1fde1dfc9a6fff86ff9f8ae6c384dffb7e1e76
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-18 22:34:34 -0400

    Update lambda_with_stdlib.cpp

--- Lambda/lambda_with_stdlib.cpp
+++ Lambda/lambda_with_stdlib.cpp
@@ -20,7 +20,12 @@
 class Person {
 public:
     Person() : _first_name(""), _last_name("") {}
-    Person(std::string first_name, std::string last_name) : _first_name(first_name), _last_name(last_name) {}
+    Person(const std::string& first_name, const std::string& last_name)
+        : _first_name(first_name)
+        , _last_name(last_name) {}
+
+    // mutator and accessor
+    // using the nature of function overloading
     std::string firstname() const { return this->_first_name; }
     std::string lastname() const { return this->_last_name; }
     void firstname(const std::string first_name) { this->_first_name = first_name; }
@@ -29,6 +34,34 @@ public:
     void print_to_stdcout() {
         std::cout << this->firstname() << " " << this->lastname() << "\n";
     };
+
+    // copy constructor
+    Person(const Person& rhs) {
+        this->firstname(rhs.firstname());
+        this->lastname(rhs.lastname());
+    }
+
+    // move constructor
+    Person(Person&& rhs) noexcept {
+        this->firstname(std::move(rhs.firstname()));
+        this->lastname(std::move(rhs.lastname()));
+    }
+
+    // copy assignment operator
+    Person operator=(const Person& rhs) {
+        return Person(rhs.firstname(), rhs.lastname());
+    }
+
+    // move assignment operator
+    Person operator=(Person&& rhs) noexcept {
+        return Person(std::move(rhs.firstname()), std::move(rhs.lastname()));
+    }
+
+    inline bool operator==(const Person& rhs) const {
+        return (this->firstname() == rhs.firstname()) && (this->lastname() == rhs.lastname());
+    }
+
+    virtual ~Person() = default;
 
 private:
     std::string _first_name;
@@ -48,7 +81,9 @@ int main(void) {
     vec_people.push_back(me);
 
     for (int i = 0; i < vec_people.size(); i++)
+    {
         vec_people[i].print_to_stdcout();
+    }
 
     // sort according to the name
     std::sort(
@@ -61,7 +96,9 @@ int main(void) {
     );
 
     for (int i = 0; i < vec_people.size(); i++)
+    {
         vec_people[i].print_to_stdcout();
+    }
 
     system("pause");
     return 0;
