e79269d9b29ea154db2df536d5cf98c6ef019cc3
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-19 22:12:06 -0400

    Create test.cpp

---
+++ Boost/Common/test.cpp
@@ -0,0 +1,28 @@
+/*****************************************************************//**
+ * \file   test.cpp
+ * \brief  Test installation of the Boost library.
+ * 
+ * $ g++ -o test.exe -IC:\\boost\\boost_1_80_0 .\test.cpp -std=c++11
+ * $ .\test.exe
+ * 
+ * \author Xuhua Huang (xuhua.huang.io@outlook.com)
+ * \date   September 2022
+ *********************************************************************/
+
+#include <iostream>
+#include <boost/version.hpp>
+#include <boost/config.hpp>
+
+int main(void) {
+    /* boost/version.hpp */
+    std::cout << BOOST_VERSION << "\n"
+        << BOOST_LIB_VERSION << "\n";
+
+    /* boost/config.hpp */
+    std::cout << BOOST_PLATFORM << "\n"
+        << BOOST_COMPILER << "\n"
+        << BOOST_STDLIB << "\n";
+
+    std::cin.get();
+    return 0;
+}
