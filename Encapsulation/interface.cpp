619600d4759c5249430401c49155ff586456182b
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-09-24 22:25:47 -0400

    Update interface.cpp

--- Encapsulation/interface.cpp
+++ Encapsulation/interface.cpp
@@ -1,17 +1,15 @@
 /*****************************************************************//**
  * \file   interface.cpp
  * \brief  pure virtual function in C++
- *		   can not instantiate instances without implementing
- *		   pure virtual function in the base class
+ *         can not instantiate instances without implementing
+ *         pure virtual function in the base class
  *
  * \author Xuhua Huang
- * \date   March 2021
+ * \date   September 24, 2022
  *********************************************************************/
 
 #include <iostream>
 #include <string>
-
-using namespace std;
 
 // class Printable only contains a pure virtual function that
 // acts like an interface keyword in other programming languages
@@ -20,37 +18,37 @@ class Printable
 public:
     // create an interface that every derived class has to implement
     // pure virtual function
-    virtual string getClassName() = 0;
+    virtual std::string getClassName() = 0;
 };
 
 // function prototype
-void printClassName(Printable* obj) {
-    cout << "Print class name: " << obj->getClassName() << endl;
+constexpr inline void printClassName(Printable* obj) {
+    std::cout << "Print class name: " << obj->getClassName() << "\n";
 }
 
 class Entity : public Printable
 {
 private:
-    string className;
+    std::string className;
 public:
     // implement defined interface
-    string getClassName() override { return "Entity : public Printable"; }
+    std::string getClassName() override { return "Entity : public Printable"; }
 };
 
 class User : public Entity
 {
 private:
-    string firstName;
-    string lastName;
+    std::string firstName;
+    std::string lastName;
 public:
     // implement defined interface
-    string getClassName() override { return "User : public Entity"; }
+    std::string getClassName() override { return "User : public Entity"; }
 
     // constructor with member initialization list
-    User(string fn, string ln) : firstName(fn), lastName(ln) {}
+    User(std::string fn, std::string ln) : firstName(fn), lastName(ln) {}
 
     // get function
-    string getUserName() { return firstName + lastName; }
+    std::string getUserName() { return firstName + lastName; }
 };
 
 int main(void)
