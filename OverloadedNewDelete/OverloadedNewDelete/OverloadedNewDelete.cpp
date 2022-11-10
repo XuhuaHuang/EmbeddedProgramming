323ba08988141e9941cbd16c537df36875c5b365
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-11-10 13:18:19 -0500

    Reformatted the file and retargetted the project.

--- OverloadedNewDelete/OverloadedNewDelete/OverloadedNewDelete.cpp
+++ OverloadedNewDelete/OverloadedNewDelete/OverloadedNewDelete.cpp
@@ -13,55 +13,55 @@ using namespace std;
 
 struct AllocationMetrics
 {
-	uint32_t TotalAllocated = 0;
-	uint32_t TotalFreed = 0;
+    uint32_t TotalAllocated = 0;
+    uint32_t TotalFreed = 0;
 
-	uint32_t CurrentUsage() { return TotalAllocated - TotalFreed; }
+    uint32_t CurrentUsage() { return TotalAllocated - TotalFreed; }
 };
 
 static AllocationMetrics metrics;
 
 void* operator new(size_t size)
 {
-	cout << "Allocating " << size << " bytes memory." << endl;
-	metrics.TotalAllocated += size;
-	return malloc(size);
+    cout << "Allocating " << size << " bytes memory." << endl;
+    metrics.TotalAllocated += size;
+    return malloc(size);
 }
 
 void operator delete(void* memory, size_t size)
 {
-	cout << "Deallocating " << size << " bytes memory." << endl;
-	metrics.TotalFreed += size;
-	free(memory);
+    cout << "Deallocating " << size << " bytes memory." << endl;
+    metrics.TotalFreed += size;
+    free(memory);
 }
 
 static void PrintMemoryUsage()
 {
-	cout << "Current memory usage " << metrics.CurrentUsage() << " bytes." << endl;
+    cout << "Current memory usage " << metrics.CurrentUsage() << " bytes." << endl;
 }
 
 struct Object
 {
-	Object() : x(0), y(0), z(0) {
-		cout << "[DFCONST]Object" << endl;
-	}
+    Object() : x(0), y(0), z(0) {
+        cout << "[DFCONST]Object" << endl;
+    }
 
-	Object(int userX, int userY, int userZ) :
-		x(userX), y(userY), z(userZ) {
-		cout << "[OLCONST]Object" << endl;
-	}
+    Object(int userX, int userY, int userZ) :
+        x(userX), y(userY), z(userZ) {
+        cout << "[OLCONST]Object" << endl;
+    }
 
-	int x, y, z;
+    int x, y, z;
 };
 
 int main(void)
 {
-	// Object *myObj = new Object();
-	PrintMemoryUsage();
-	{
-		unique_ptr<Object> obj_ptr = make_unique<Object>();
-		PrintMemoryUsage();
-	}
-	PrintMemoryUsage();
-	return 0;
+    // Object *myObj = new Object();
+    PrintMemoryUsage();
+    {
+        unique_ptr<Object> obj_ptr = make_unique<Object>();
+        PrintMemoryUsage();
+    }
+    PrintMemoryUsage();
+    return 0;
 }
