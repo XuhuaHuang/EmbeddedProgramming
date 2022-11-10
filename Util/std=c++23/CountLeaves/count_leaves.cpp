5893cee472b58370b0d4f259ae9e2b9030ad4746
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-11-10 14:51:13 -0500

    Created algorithm to count the leaves of a binary tree.

---
+++ Util/std=c++23/CountLeaves/count_leaves.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file count_leaves.cpp
+ * @author Xuhua Huang (xuhua.huang.io@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-11-10
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+#include <variant>
+
+struct Leaf {};
+/* forward declaration */
+struct Node;
+
+using Tree = std::variant<Leaf, Node*>;
+
+struct Node {
+    Tree left, right;
+};
+
+template <typename... Ts> struct overload : Ts... {
+    using Ts::operator()...;
+};
+
+int count_leaves(const Tree &tree) {
+    return std::visit(overload{
+        [](const Leaf &) { return 1; },
+        [](this const auto &self, const Node *node) -> int {
+            return visit(self, node->left) + visit(self, node->right);
+        }
+    }, tree);
+}
+
+int main(void) {
+    // empty main function for the sake of compilation
+    return 0;
+}
