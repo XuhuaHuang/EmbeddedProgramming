8333d40965fc113dab9a150d035388ae28850c8a
XuhuaHuang <xuhua.huang.io@gmail.com>
2022-11-11 17:17:40 -0500

    Updated solution file with CMake instructions.

--- MemberFnAmpersand/MemberFnAmpersand/MemberFnAmpersand.cpp
+++ MemberFnAmpersand/MemberFnAmpersand/MemberFnAmpersand.cpp
@@ -1,24 +1,32 @@
+/*****************************************************************//**
+ * \file   MemberFnAmpersand.cpp
+ * \brief  
+ * 
+ * \author Xuhua Huang
+ * \date   November 2022
+ *********************************************************************/
+
 #include <iostream>
 
 struct Arg
 {
-	int i = 1;
-	Arg(int _i) : i {_i} {}
-	int getArg()&& {
-		std::cout << "In member function int getArg() &&" << std::endl;
-		return i;
-	}
+    int i = 1;
+    Arg(int _i) : i {_i} {}
+    int getArg()&& {
+        std::cout << "In member function int getArg() &&" << std::endl;
+        return i;
+    }
 
-	int& getArg()& {
-		std::cout << "In member function int& getArg() &" << std::endl;
-		return i;
-	}
+    int& getArg()& {
+        std::cout << "In member function int& getArg() &" << std::endl;
+        return i;
+    }
 };
 
 int main(void)
 {
-	Arg g1{ 5 };							 /* instanciation with innitializer_list */
-	std::cout << g1.getArg() << std::endl;				/* expecting int& getArg() & */
-	std::cout << std::move(g1).getArg() << std::endl;	/* expecting int getArg() && */
-	return 0;
+    Arg g1{ 5 };                              /* instanciation with innitializer_list */
+    std::cout << g1.getArg() << std::endl;               /* expecting int& getArg() & */
+    std::cout << std::move(g1).getArg() << std::endl;    /* expecting int getArg() && */
+    return 0;
 }
