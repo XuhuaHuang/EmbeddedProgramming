c9fe1cc05e8e200c8eb6e497f03dfb9dd7bcb090
XuhuaHuang <xuhua.huang.io@gmail.com>
2022-11-11 16:15:48 -0500

    Reformatted the files.

--- OverloadedFunctions/mytime.cpp
+++ OverloadedFunctions/mytime.cpp
@@ -5,88 +5,88 @@ using namespace std;
 
 void mytime::setHour (int userHour) // hour should be valid form 0 to 23
 {
-	if ((userHour >= 0) && (userHour <= 23))
-	{
-		hour = userHour;
-	}	
-	else if (userHour == 24)
-	{
-		cout << "Do you mean 0 a.m.? It is automatically set to 0 a.m." << endl;
-		hour = 0;
-	}
-	else
-	{
-		cout << "Invalid value " << userHour << endl;
-		hour = 0;
-	}
-	
-	return;
+    if ((userHour >= 0) && (userHour <= 23))
+    {
+        hour = userHour;
+    }
+    else if (userHour == 24)
+    {
+        cout << "Do you mean 0 a.m.? It is automatically set to 0 a.m." << endl;
+        hour = 0;
+    }
+    else
+    {
+        cout << "Invalid value " << userHour << endl;
+        hour = 0;
+    }
+
+    return;
 }
 
 void mytime::setMinute (int userMinute) // minute should be valid from 0 to 59
 {
-	if ((userMinute >= 0) && (userMinute <= 59))
-	{
-		minute = userMinute;
-	}	
-	else
-	{
-		cout << "Invalid value " << userMinute << endl;
-		minute = 0;
-	}
-	
-	return;
+    if ((userMinute >= 0) && (userMinute <= 59))
+    {
+        minute = userMinute;
+    }
+    else
+    {
+        cout << "Invalid value " << userMinute << endl;
+        minute = 0;
+    }
+
+    return;
 }
 
 void mytime::setSecond (int userSecond) // second should be valid form 0 to 59
 {
-	if ((userSecond >= 0) && (userSecond<= 59))
-	{
-		second = userSecond;
-	}	
-	else
-	{
-		cout << "Invalid value " << userSecond << endl;
-		second = 0;
-	}
-	
-	return;
+    if ((userSecond >= 0) && (userSecond<= 59))
+    {
+        second = userSecond;
+    }
+    else
+    {
+        cout << "Invalid value " << userSecond << endl;
+        second = 0;
+    }
+
+    return;
 }
 
 void mytime::setTime(int userHour, int userMinute, int userSecond) // original set time functions with three variables
 {
-	setHour(userHour);
-	setMinute(userMinute);
-	setSecond(userSecond);
-	
-	return;
+    setHour(userHour);
+    setMinute(userMinute);
+    setSecond(userSecond);
+
+    return;
 }
 
 void mytime::printTime()
 {
-	cout << getHour() << ":" << getMinute() << ":" << getSecond() << endl; // using get functions to get value with verification
-	
-	return;
+    cout << getHour() << ":" << getMinute() << ":" << getSecond() << endl; // using get functions to get value with verification
+
+    return;
 }
 
 
 void mytime::setTime(int userHour, int userMinute) // overloaded function #1
 {
-	setHour(userHour);
-	setMinute(userMinute);
-	
-	setSecond(0); // to keep the variable from garbage variable
-	
-	return;
+    setHour(userHour);
+    setMinute(userMinute);
+    
+    setSecond(0); // to keep the variable from garbage variable
+
+    return;
 }
 
 
 void mytime::setTime(int userHour) // overloaded function #2
 {
-	setHour(userHour);
-	
-	setMinute(0); // set default value 0 for testing
-	setSecond(0);
-	
-	return;
+    setHour(userHour);
+    
+    setMinute(0); // set default value 0 for testing
+    setSecond(0);
+
+    return;
 }
--- OverloadedFunctions/mytimemain.cpp
+++ OverloadedFunctions/mytimemain.cpp
@@ -5,47 +5,46 @@ using namespace std;
 
 int main (void)
 {
+    mytime userTime; // declaring an object
 
-	mytime userTime; // declaring an object
-	
-	int userHour;
-	cout << "Please enter the hour you want to set to." << endl;
-	cin >> userHour;
-	userTime.setHour(userHour); // call the set function with verification
-	
-	int userMinute;
-	cout << "Please enter the minute you want to set to." << endl;
-	cin >> userMinute;
-	userTime.setMinute(userMinute); 
-	
-	int userSecond;
-	cout << "Please enter the second you want to set to." << endl;
-	cin >> userSecond;
-	userTime.setSecond(userSecond); 
-	
-	userTime.printTime(); // this line should print the time user sets to
-	// asking the user to set section ends
-	
-	// test cases and print function
-	cout << "Test case 1: valid hour and minute." << endl;
-	userTime.setTime(12,34); // valid hour, minute; should call mytime::setTime(int , int)
-	userTime.printTime(); // 12:34:00
-	
-	cout << "Test case 2: invalid hour and valid minute." << endl;
-	userTime.setTime(25, 34); // invalid hour and valid minute; should call mytime::setTime(int, int)
-	userTime.printTime(); // 00:34:00
-	
-	cout << "Test case 3: valid hour and invalid minute." << endl;
-	userTime.setTime(12, 60); // valid hour and invalid minute; should call mytime::setTime(int, int)	
-	userTime.printTime(); // 12:00:00
+    int userHour;
+    cout << "Please enter the hour you want to set to." << endl;
+    cin >> userHour;
+    userTime.setHour(userHour); // call the set function with verification
 
-	cout << "Test case 4: valid hour4." << endl;
-	userTime.setTime(23); // valid hour; should call mytime::setTime(int userHour);
-	userTime.printTime(); // 23:00:00
-	
-	cout << "Test case 5: invalid hour." << endl;
-	userTime.setTime(25); // invalid hour; should call mytime::setTime(int userHour);
-	userTime.printTime(); // 00:00:00
-	
-	return 0;
+    int userMinute;
+    cout << "Please enter the minute you want to set to." << endl;
+    cin >> userMinute;
+    userTime.setMinute(userMinute); 
+
+    int userSecond;
+    cout << "Please enter the second you want to set to." << endl;
+    cin >> userSecond;
+    userTime.setSecond(userSecond); 
+
+    userTime.printTime(); // this line should print the time user sets to
+    // asking the user to set section ends
+
+    // test cases and print function
+    cout << "Test case 1: valid hour and minute." << endl;
+    userTime.setTime(12,34); // valid hour, minute; should call mytime::setTime(int , int)
+    userTime.printTime(); // 12:34:00
+
+    cout << "Test case 2: invalid hour and valid minute." << endl;
+    userTime.setTime(25, 34); // invalid hour and valid minute; should call mytime::setTime(int, int)
+    userTime.printTime(); // 00:34:00
+
+    cout << "Test case 3: valid hour and invalid minute." << endl;
+    userTime.setTime(12, 60); // valid hour and invalid minute; should call mytime::setTime(int, int)	
+    userTime.printTime(); // 12:00:00
+
+    cout << "Test case 4: valid hour4." << endl;
+    userTime.setTime(23); // valid hour; should call mytime::setTime(int userHour);
+    userTime.printTime(); // 23:00:00
+
+    cout << "Test case 5: invalid hour." << endl;
+    userTime.setTime(25); // invalid hour; should call mytime::setTime(int userHour);
+    userTime.printTime(); // 00:00:00
+
+    return 0;
 }
