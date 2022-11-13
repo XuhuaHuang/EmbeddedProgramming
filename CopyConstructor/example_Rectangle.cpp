92f4502a2bf0edecaefae2179bffb53427f7e2ac
XuhuaHuang <xuhua.huang.io@gmail.com>
2022-11-12 23:07:21 -0500

    Reformatted directory.

--- CopyConstructor/example_Rectangle.cpp
+++ CopyConstructor/example_Rectangle.cpp
@@ -5,51 +5,49 @@
  * A copy constructor is a special constructor called whenever
  * a new object is created and initialized with another object's data.
  * 
- * \author Xuhua
+ * \author Xuhua Huang
  * \date   November 2020
  *********************************************************************/
 
 #include <iostream>
-using namespace std;
 
 class Rectangle
 {
 private:
-	int width, height;
+    int width, height;
 
 public:
+    // set and get functions
+    void setWidth(const int userWidth) { width = userWidth; }
+    int getWidth(void) { return width; }
 
-	// set and get functions
-	void setWidth(const int userWidth) { width = userWidth; }
-	int getWidth(void) { return width; }
+    void setHeight(const int userHeight) { height = userHeight; }
+    int getHeight(void) { return height; }
 
-	void setHeight(const int userHeight) { height = userHeight; }
-	int getHeight(void) { return height; }
+    // other functions
+    int area() { return (width * height); }
 
-	// other functions
-	int area() { return (width * height); }
-
-	// overloaded constructor
-	Rectangle(const int initWidth, const int initHeight) : width(initWidth), height(initHeight) {}
-	// copy constructor
-	Rectangle(const Rectangle& rectangle);
+    // overloaded constructor
+    Rectangle(const int initWidth, const int initHeight) : width(initWidth), height(initHeight) {}
+    // copy constructor
+    Rectangle(const Rectangle& rectangle);
 };
 
 // copy constructor
 Rectangle::Rectangle(const Rectangle& rectangle)
 {
-	this->setWidth(rectangle.width);
-	this->setHeight(rectangle.height);
+    this->setWidth(rectangle.width);
+    this->setHeight(rectangle.height);
 }
 
 
 int main(void)
 {
-	Rectangle rect(4, 5); // calls overloaded constructor
-	Rectangle anotherRect(rect); // calls copy constructor
+    Rectangle rect(4, 5); // calls overloaded constructor
+    Rectangle anotherRect(rect); // calls copy constructor
 
-	cout << rect.area() << endl;  // outputs 20
-	cout << anotherRect.area() << endl; // outputs 20
+    std::cout << rect.area() << "\n";  // outputs 20
+    std::cout << anotherRect.area() << "\n"; // outputs 20
 
-	return 0;
+    return 0;
 }
