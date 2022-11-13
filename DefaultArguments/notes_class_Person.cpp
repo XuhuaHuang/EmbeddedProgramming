0407cd5d0db32150934c6e1c8d1402d3274a7638
XuhuaHuang <xuhua.huang.io@gmail.com>
2022-11-12 23:16:37 -0500

    Reformatted directory and added CMake instructions.

--- DefaultArguments/notes_class_Person.cpp
+++ DefaultArguments/notes_class_Person.cpp
@@ -1,60 +1,57 @@
 /*****************************************************************//**
  * \file   default_arguments_notes.cpp
  * \brief  In-class example, provide an example on default argument
- * 
- * Things to know: When a function has a mixture of parameters 
- * both with and without default arguments 
+ *
+ * Things to know: When a function has a mixture of parameters
+ * both with and without default arguments
  * parameters with default arguments must be declared last.
- * 
+ *
  * \author Xuhua
  * \date   November 2020
  *********************************************************************/
 
 #include <iostream>
 #include <string>
-#include <vector>
-
-using namespace std;
 
 class Person
 {
 public:
-	// get and set functions for private attributes
-	void setName(string userName) { name = userName; }
-	string getName() { return name; }
+    // get and set functions for private attributes
+    void setName(std::string userName) { name = userName; }
+    std::string getName() { return name; }
 
-	void setAge(int userAge) { age = userAge; }
-	int getAge() { return age; }
+    void setAge(int userAge) { age = userAge; }
+    int getAge() { return age; }
 
-	// other functions:
-	void setData(string userUame, int age = 12);
-	void printData();
+    // other functions:
+    void setData(std::string userUame, int age = 12);
+    void printData();
 
 private:
-	string name;
-	int age;
+    std::string name;
+    int age;
 };
 
-void Person::setData(string userName, int age)
+void Person::setData(std::string userName, int age)
 {
-	setName(userName);
-	setAge(age);
+    setName(userName);
+    setAge(age);
 }
 
 void Person::printData()
 {
-	cout << "Person's name is: " << name << " of " << age << " years old." << endl;
+    std::cout << "Person's name is: " << name << " of " << age << " years old." << "\n";
 }
 
 int main(void)
 {
-	Person person1;
-	person1.setData("hello", 24); // overwrite the age to 24
-	person1.printData(); // print age = 24
+    Person person1;
+    person1.setData("hello", 24); // overwrite the age to 24
+    person1.printData(); // print age = 24
 
-	Person person2;
-	person2.setData("world"); // age is default to 12
-	person2.printData(); // print age = 12
+    Person person2;
+    person2.setData("world"); // age is default to 12
+    person2.printData(); // print age = 12
 
-	return 0;
+    return 0;
 }
