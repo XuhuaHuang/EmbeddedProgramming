76afc276e7903da94a108670c2bf9d8396c8633c
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-11-12 23:16:37 -0500

    Reformatted directory and added CMake instructions.

--- DefaultArguments/example_Person.cpp
+++ DefaultArguments/example_Person.cpp
@@ -1,11 +1,11 @@
 /*****************************************************************//**
  * \file   example_Person.cpp
  * \brief  In-class example, provide an example on default argument
- * 
- * Things to know: When a function has a mixture of parameters 
- * both with and without default arguments 
+ *
+ * Things to know: When a function has a mixture of parameters
+ * both with and without default arguments
  * parameters with default arguments must be declared last.
- * 
+ *
  * \author Xuhua
  * \date   November 2020
  *********************************************************************/
@@ -14,54 +14,52 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 class Person
 {
 public:
-	// get and set functions for private attributes
-	void setName(string userName) { name = userName; }
-	string getName() { return name; }
+    // get and set functions for private attributes
+    void setName(std::string userName) { name = userName; }
+    std::string getName() { return name; }
 
-	void setAge(int userAge) { age = userAge; }
-	int getAge() { return age; }
+    void setAge(int userAge) { age = userAge; }
+    int getAge() { return age; }
 
-	// other functions:
-	void setData(string userUame, int age = 12);
-	void printData();
+    // other functions:
+    void setData(std::string userUame, int age = 12);
+    void printData();
 
 private:
-	string name;
-	int age;
+    std::string name;
+    int age;
 };
 
-void Person::setData(string userName, int age)
+void Person::setData(std::string userName, int age)
 {
-	setName(userName);
-	setAge(age);
+    setName(userName);
+    setAge(age);
 }
 
 void Person::printData()
 {
-	cout << "Person's name is: " << name << " of " << age << " years old." << endl;
+    std::cout << "Person's name is: " << name << " of " << age << " years old." << "\n";
 }
 
 int main(void)
 {
-	// test case 1
-	Person person1;
-	person1.setData("hello", 24); // overwrite the age to 24
-	person1.printData(); // print age = 24
-	
-	/*
-	person1.setData("Charles"); // age will be set to 12
-	person1.printData(); // Charles, 12
-	*/
-	
-	// test case 2
-	Person person2;
-	person2.setData("world"); // age is default to 12
-	person2.printData(); // print age = 12
+    // test case 1
+    Person person1;
+    person1.setData("hello", 24); // overwrite the age to 24
+    person1.printData(); // print age = 24
 
-	return 0;
+    /*
+    person1.setData("Charles"); // age will be set to 12
+    person1.printData(); // Charles, 12
+    */
+
+    // test case 2
+    Person person2;
+    person2.setData("world"); // age is default to 12
+    person2.printData(); // print age = 12
+
+    return 0;
 }
--- DefaultArguments/lab_Schedule.cpp
+++ DefaultArguments/lab_Schedule.cpp
@@ -1,7 +1,7 @@
 /*****************************************************************//**
  * \file   lab_Schedule.cpp
  * \brief  problem 1 with default arguments with example class Schedule
- *  
+ *
  * \author Xuhua
  * \date   November 2020
  *********************************************************************/
@@ -9,56 +9,54 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 class Schedule
 {
 public:
 
-	// set and get functions
-	void setCourseName(string userCourseName) { courseName = userCourseName; }
-	string getCourseName() { return courseName; }
+    // set and get functions
+    void setCourseName(std::string userCourseName) { courseName = userCourseName; }
+    std::string getCourseName() { return courseName; }
 
-	void setStudentID(int userStudentID) { studentID = userStudentID; }
-	int getStudentID() { return studentID; }
+    void setStudentID(int userStudentID) { studentID = userStudentID; }
+    int getStudentID() { return studentID; }
 
-	// other function prototype
-	void setInfo(string userCourseName = "Unknown", int userStudentID = 0);
-	void printSchedule() { 
-		cout << "\nCourse " << courseName 
-			 << " is assigned to student "<< studentID << "." << endl;
-	}
+    // other function prototype
+    void setInfo(std::string userCourseName = "Unknown", int userStudentID = 0);
+    void printSchedule() {
+        std::cout << "\nCourse " << courseName
+            << " is assigned to student " << studentID << "." << "\n";
+    }
 
 private:
-	string courseName;
-	int studentID;
+    std::string courseName;
+    int studentID;
 };
 
-void Schedule::setInfo(string userCourseName, int userStudentID)
+void Schedule::setInfo(std::string userCourseName, int userStudentID)
 {
-	setCourseName(userCourseName);
-	setStudentID(userStudentID);
+    setCourseName(userCourseName);
+    setStudentID(userStudentID);
 }
 
 int main(void)
 {
-	// test case 1
-	// pass setInfo() both courseName and studentID
-	Schedule fall2020;
-	fall2020.setInfo("Embedded Programming", 1879700);
-	fall2020.printSchedule();
+    // test case 1
+    // pass setInfo() both courseName and studentID
+    Schedule fall2020;
+    fall2020.setInfo("Embedded Programming", 1879700);
+    fall2020.printSchedule();
 
-	// test case 2
-	// do not pass any arguments
-	Schedule emptySchedule;
-	emptySchedule.setInfo();
-	emptySchedule.printSchedule(); // 'Unknown' and 0
+    // test case 2
+    // do not pass any arguments
+    Schedule emptySchedule;
+    emptySchedule.setInfo();
+    emptySchedule.printSchedule(); // 'Unknown' and 0
 
-	// test case 3
-	// pass only the courseName
-	Schedule courseNameOnly;
-	courseNameOnly.setInfo("Final project I");
-	courseNameOnly.printSchedule(); // studentID is default to 0
+    // test case 3
+    // pass only the courseName
+    Schedule courseNameOnly;
+    courseNameOnly.setInfo("Final project I");
+    courseNameOnly.printSchedule(); // studentID is default to 0
 
-	return 0;
+    return 0;
 }
