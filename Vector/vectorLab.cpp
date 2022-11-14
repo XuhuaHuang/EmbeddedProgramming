68c18f292b4d5dd003770488b75f0a9a7dad5c5f
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-11-14 09:17:54 -0500

    Reformatted code in Vector directory with CMake instructions.

--- Vector/vectorLab.cpp
+++ Vector/vectorLab.cpp
@@ -1,67 +1,72 @@
+/*****************************************************************//**
+ * \file   vectorLab.cpp
+ * \brief  Lab on std::vector<typename T> use cases and
+ * std::vector<typename T>::iterator use cases
+ *
+ * \author Xuhua Huang
+ * \date   October 13, 2020
+ *********************************************************************/
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <random>
 
-// 1879700 Xuhua Huang
-
-using namespace std;
-
 // function prototype:
-void printIntVector(vector<int> intv);
-void printCharVector(vector<char> charv);
-void squareAndPrint(int x);
-void palindrome(vector<int> vint);
-void palindrome(vector<string> vstring);
-void palindrome(vector<char> vchar);
+void printIntVector(std::vector<int>);
+void printCharVector(std::vector<char>);
+void squareAndPrint(int);
+void palindrome(std::vector<int>);
+void palindrome(std::vector<std::string>);
+void palindrome(std::vector<char>);
 
-int main (void)
+int main(void)
 {
-    
-    vector<int> v1(2, 0); // vector v1 of type int
-    cout << "The first element of v1 is: " << v1.at(0) << endl; // print the first element 0
-    
+
+    std::vector<int> v1(2, 0); // std::vector v1 of type int
+    std::cout << "The first element of v1 is: " << v1.at(0) << "\n"; // print the first element 0
+
     v1.at(1) = 4;
-    cout << "The second element of v1 is: " << v1.at(1) << endl; // make sure the first element is set to 4
-    
+    std::cout << "The second element of v1 is: " << v1.at(1) << "\n"; // make sure the first element is set to 4
+
     v1.push_back(3); // adding elements sequentially
     v1.push_back(5);
     v1.push_back(16);
     v1.push_back(8);
     v1.push_back(2);
     v1.push_back(7);
-    
-    // string iterator string::iterator iter;
-    // vector<int>::iterator iterv1; // vector<datatype>::iterator iterName;
-    printIntVector(v1);	
-    cout << "The size of vector v1 is: " << v1.size() << endl << endl;
-    
-    v1.resize(10,0);
+
+    // std::string iterator std::string::iterator iter;
+    // std::vector<int>::iterator iterv1; // std::vector<datatype>::iterator iterName;
+    printIntVector(v1);
+    std::cout << "The size of std::vector v1 is: " << v1.size() << "\n" << "\n";
+
+    v1.resize(10, 0);
     printIntVector(v1);							// two zeros at the end as recently added element
-    cout << "The size of vector v1 is now: " << v1.size() << endl << endl;
-    
+    std::cout << "The size of std::vector v1 is now: " << v1.size() << "\n" << "\n";
+
     v1.pop_back(); // delete the last element
     printIntVector(v1);
-    cout << "The size of vector v1 is now: " << v1.size() << endl; // should print 9
-    
+    std::cout << "The size of std::vector v1 is now: " << v1.size() << "\n"; // should print 9
+
     v1.clear();
-    cout << "\nClearing..." << endl;
-    printIntVector(v1); // empty vector -> nothing
-    cout << "The size of vector v1 is now: " << v1.size() << endl; // should print 9
-    
+    std::cout << "\nClearing..." << "\n";
+    printIntVector(v1); // empty std::vector -> nothing
+    std::cout << "The size of std::vector v1 is now: " << v1.size() << "\n"; // should print 9
+
     /*
-    cout << "Sorting..." << endl;
+    std::cout << "Sorting..." << "\n";
     sort(v1.begin(), v1.end()); // sort function
-    printIntVector(v1); // print sorted acceding vector
-    
-    cout << "Reversing..." << endl;
+    printIntVector(v1); // print sorted acceding std::vector
+
+    std::cout << "Reversing..." << "\n";
     reverse(v1.begin(), v1.end()); // reverse function
-    printIntVector(v1); // print reversed vector
+    printIntVector(v1); // print reversed std::vector
     */
-    
-    vector<int> vA {1,2,3,4,5,2,7,4,1}; // creating int vector named 'vA'
-    // vector<datatype> vectorName {num1, num2}; 
+
+    std::vector<int> vA{ 1,2,3,4,5,2,7,4,1 }; // creating int std::vector named 'vA'
+    // std::vector<datatype> std::vectorName {num1, num2}; 
     /*
     vA.push_back(1);
     vA.push_back(2);
@@ -71,17 +76,17 @@ int main (void)
     vA.push_back(2);
     vA.push_back(7);
     vA.push_back(4);
-    vA.push_back(1); // writing all elements into the vector
+    vA.push_back(1); // writing all elements into the std::vector
     */
-    cout << "\nSorting..." << endl;
+    std::cout << "\nSorting..." << "\n";
     sort(vA.begin(), vA.end());
     printIntVector(vA); // print sorted
-    
-    cout << "\nReversing..." << endl;
+
+    std::cout << "\nReversing..." << "\n";
     reverse(vA.begin(), vA.end());
     printIntVector(vA); // print reversed
-    
-    vector<char> vB {'B','S','T', 'W','S','K','A','W'}; // creating int vector named 'vB'
+
+    std::vector<char> vB{ 'B','S','T', 'W','S','K','A','W' }; // creating int std::vector named 'vB'
     /*
     vB.push_back('B');
     vB.push_back('S');
@@ -90,159 +95,159 @@ int main (void)
     vB.push_back('S');
     vB.push_back('K');
     vB.push_back('A');
-    vB.push_back('W'); // writing all characters to the char vector
+    vB.push_back('W'); // writing all characters to the char std::vector
     */
-    
-    cout << "\nSorting..." << endl;
+
+    std::cout << "\nSorting..." << "\n";
     sort(vB.begin(), vB.end());
     printCharVector(vB); // print sorted
-    
-    cout << "\nReversing..." << endl;
+
+    std::cout << "\nReversing..." << "\n";
     reverse(vB.begin(), vB.end());
     printCharVector(vB); // print reversed
-    
-    cout << "\nShuffling..." << endl;
+
+    std::cout << "\nShuffling..." << "\n";
     random_shuffle(vB.begin(), vB.end());
     printCharVector(vB); // print shuffled
-    
-    // counting in vector A:
-    cout << "\nIn vector A:" << endl;
-    cout << "The times element \"1\" is: " << count(vA.begin(), vA.end(), 1) << endl;
-    cout << "The times element \"2\" is: " << count(vA.begin(), vA.end(), 2) << endl;
-    cout << "The times element \"3\" is: " << count(vA.begin(), vA.end(), 3) << endl;
-    cout << "The times element \"4\" is: " << count(vA.begin(), vA.end(), 4) << endl;
-    
-    // counting in vector B:
-    cout << "\nIn vector B:" << endl;
-    cout << "The times element \"A\" is: " << count(vB.begin(), vB.end(), 'A') << endl;
-    cout << "The times element \"B\" is: " << count(vB.begin(), vB.end(), 'B') << endl;
-    cout << "The times element \"S\" is: " << count(vB.begin(), vB.end(), 'S') << endl;
-    cout << "The times element \"W\" is: " << count(vB.begin(), vB.end(), 'W') << endl;
-    
-    // max_elementin both vectors:
-    cout << "\nConsulting the biggest element in both vectors..." << endl;
-    cout << "The max element is vector A is: " << *max_element(vA.begin(), vA.end()) << endl; // print 7
-    cout << "The max element is vector B is: " << *max_element(vB.begin(), vB.end()) << endl; // print 'W'
-    
-    // min_elementin both vectors:
-    cout << "\nConsulting the smallest element in both vectors..." << endl;
-    cout << "The min element is vector A is: " << *min_element(vA.begin(), vA.end()) << endl; // print 1
-    cout << "The min element is vector B is: " << *min_element(vB.begin(), vB.end()) << endl; // print 'A'
-    
-    // replace elements in both vectors:
-    cout << "\nReplacing all \"1\"s in vector A..." << endl;
+
+    // counting in std::vector A:
+    std::cout << "\nIn std::vector A:" << "\n";
+    std::cout << "The times element \"1\" is: " << count(vA.begin(), vA.end(), 1) << "\n";
+    std::cout << "The times element \"2\" is: " << count(vA.begin(), vA.end(), 2) << "\n";
+    std::cout << "The times element \"3\" is: " << count(vA.begin(), vA.end(), 3) << "\n";
+    std::cout << "The times element \"4\" is: " << count(vA.begin(), vA.end(), 4) << "\n";
+
+    // counting in std::vector B:
+    std::cout << "\nIn std::vector B:" << "\n";
+    std::cout << "The times element \"A\" is: " << count(vB.begin(), vB.end(), 'A') << "\n";
+    std::cout << "The times element \"B\" is: " << count(vB.begin(), vB.end(), 'B') << "\n";
+    std::cout << "The times element \"S\" is: " << count(vB.begin(), vB.end(), 'S') << "\n";
+    std::cout << "The times element \"W\" is: " << count(vB.begin(), vB.end(), 'W') << "\n";
+
+    // max_elementin both std::vectors:
+    std::cout << "\nConsulting the biggest element in both std::vectors..." << "\n";
+    std::cout << "The max element is std::vector A is: " << *max_element(vA.begin(), vA.end()) << "\n"; // print 7
+    std::cout << "The max element is std::vector B is: " << *max_element(vB.begin(), vB.end()) << "\n"; // print 'W'
+
+    // min_elementin both std::vectors:
+    std::cout << "\nConsulting the smallest element in both std::vectors..." << "\n";
+    std::cout << "The min element is std::vector A is: " << *min_element(vA.begin(), vA.end()) << "\n"; // print 1
+    std::cout << "The min element is std::vector B is: " << *min_element(vB.begin(), vB.end()) << "\n"; // print 'A'
+
+    // replace elements in both std::vectors:
+    std::cout << "\nReplacing all \"1\"s in std::vector A..." << "\n";
     replace(vA.begin(), vA.end(), 1, 8); // replace all 1s with 8s
     sort(vA.begin(), vA.end()); // sort before printing
     printIntVector(vA); // no 1s.
-    
-    cout << "\nReplacing all \"S\"s in vector B..." << endl;
+
+    std::cout << "\nReplacing all \"S\"s in std::vector B..." << "\n";
     replace(vB.begin(), vB.end(), 'S', 'C'); // replace all 'S' with 'C'
     sort(vB.begin(), vB.end());
     printCharVector(vB);
-    
+
     //squareAndPrint(9); // testing function
     // for_each function
-    cout << "\nSquaring vector A..." << endl;
+    std::cout << "\nSquaring std::vector A..." << "\n";
     for_each(vA.begin(), vA.end(), squareAndPrint); // only the function name
-    
-    // checking if it's palindrome int vectors:
-    vector<int> testint {1,2,3,2,1};
+
+    // checking if it's palindrome int std::vectors:
+    std::vector<int> testint{ 1,2,3,2,1 };
     palindrome(testint);
-    
-    vector<int> testint2 {1,2,3,4,5};
+
+    std::vector<int> testint2{ 1,2,3,4,5 };
     palindrome(testint2);
-    
-    // checking if it's palindrome string vectors: 
-    string testword = "HELLO"; // not a palindrome
-    vector<string> teststring { testword };
-    palindrome(teststring); // should print not a palindrome
-    
-    // for vector<string>, I think the reason why it does NOT work is I had to go into the first element and reverse the element.
-    // instead of reversing the string vector
-    
-    string testword2 = "ANNAA";
-    vector<string> teststring2 { testword2 };
-    palindrome(teststring2); // should print is a palindrome
-    
-    vector<char> chartest1 {'H','E','L','L','O' };
-    cout << "\nThe word HELLO" << endl;
+
+    // checking if it's palindrome std::string std::vectors: 
+    std::string testword = "HELLO"; // not a palindrome
+    std::vector<std::string> testString{ testword };
+    palindrome(testString); // should print not a palindrome
+
+    // for std::vector<std::string>, I think the reason why it does NOT work is I had to go into the first element and reverse the element.
+    // instead of reversing the std::string std::vector
+
+    std::string testword2 = "ANNAA";
+    std::vector<std::string> testString2{ testword2 };
+    palindrome(testString2); // should print is a palindrome
+
+    std::vector<char> chartest1{ 'H','E','L','L','O' };
+    std::cout << "\nThe word HELLO" << "\n";
     palindrome(chartest1); // not a palindrome
-    
-    vector<char> chartest2 { 'A','N','N','A' };
-    cout << "The word ANNA" << endl;
+
+    std::vector<char> chartest2{ 'A','N','N','A' };
+    std::cout << "The word ANNA" << "\n";
     palindrome(chartest2); // a palindrome
-    
+
     return 0;
 }
 
 
-void printIntVector(vector<int> intv)
+void printIntVector(std::vector<int> intv)
 {
-    vector<int>::const_iterator iterint;
-    cout << "The content of this integer vector is listed: " << endl;
-    
-    for(iterint = intv.begin(); iterint < intv.end(); iterint++)
+    std::vector<int>::const_iterator iterint;
+    std::cout << "The content of this integer std::vector is listed: " << "\n";
+
+    for (iterint = intv.begin(); iterint < intv.end(); iterint++)
     {
-        cout << *iterint << endl;
+        std::cout << *iterint << "\n";
     } // end for
-    
-    cout << "This is the end of the integer vector." << endl;
+
+    std::cout << "This is the end of the integer std::vector." << "\n";
 }
 
-void printCharVector(vector<char> charv)
+void printCharVector(std::vector<char> charv)
 {
-    vector<char>::const_iterator iterchar;
-    cout << "The content of this character vector is listed: " << endl;
-    
-    for(iterchar = charv.begin(); iterchar < charv.end(); iterchar++)
+    std::vector<char>::const_iterator iterchar;
+    std::cout << "The content of this character std::vector is listed: " << "\n";
+
+    for (iterchar = charv.begin(); iterchar < charv.end(); iterchar++)
     {
-        cout << *iterchar << endl; // print v3 using vectors
+        std::cout << *iterchar << "\n"; // print v3 using std::vectors
     } // end for
-    
-    cout << "This is the end of the integer vector." << endl;	
+
+    std::cout << "This is the end of the integer std::vector." << "\n";
 }
 
 void squareAndPrint(int x)
 {
-    cout << "Squared: " << (x*x) << endl; // x^2 did NOT work not sure why
+    std::cout << "Squared: " << (x * x) << "\n"; // x^2 did NOT work not sure why
     return;
 }
 
-void palindrome(vector<int> vint)
+void palindrome(std::vector<int> vint)
 {
-    vector<int> vintCopy = vint; // copy to a new vector for comparison
-    reverse( vintCopy.begin(), vintCopy.end() );
-    
-    if(vintCopy == vint)
-        cout << "\nThis integer vector is a Palindrome." << endl;
+    std::vector<int> vintCopy = vint; // copy to a new std::vector for comparison
+    reverse(vintCopy.begin(), vintCopy.end());
+
+    if (vintCopy == vint)
+        std::cout << "\nThis integer std::vector is a Palindrome." << "\n";
     else
-        cout << "\nThis integer vector is not a Palindrome." << endl;
+        std::cout << "\nThis integer std::vector is not a Palindrome." << "\n";
     return;
 }
 
-void palindrome(vector<string> vstring)
+void palindrome(std::vector<std::string> vString)
 {
-    vector<string> vstringCopy = vstring; // copy to a new vector for comparison
-    // cout << vstringCopy.at(0) << endl; // test
-    reverse( vstringCopy.begin(), vstringCopy.end() );
-    // cout << vstringCopy.at(0) << endl; // test
-    
-    if(vstringCopy == vstring)
-        cout << "\nThis string vector is a Palindrome." << endl;
+    std::vector<std::string> vStringCopy = vString; // copy to a new std::vector for comparison
+    // std::cout << vstd::stringCopy.at(0) << "\n"; // test
+    std::reverse(vStringCopy.begin(), vStringCopy.end());
+    // std::cout << vstd::stringCopy.at(0) << "\n"; // test
+
+    if (vStringCopy == vStringCopy)
+        std::cout << "\nThis std::string std::vector is a Palindrome." << "\n";
     else
-        cout << "\nThis string vector is not a Palindrome." << endl;
+        std::cout << "\nThis std::string std::vector is not a Palindrome." << "\n";
     return;
 }
 
-void palindrome(vector<char> vchar)
+void palindrome(std::vector<char> vchar)
 {
-    vector<char> vcharCopy = vchar;
-    reverse( vcharCopy.begin(), vcharCopy.end() );
-    
-    if(vcharCopy == vchar)
-        cout << "\nThis string vector is a Palindrome." << endl;
+    std::vector<char> vcharCopy = vchar;
+    reverse(vcharCopy.begin(), vcharCopy.end());
+
+    if (vcharCopy == vchar)
+        std::cout << "\nThis std::string std::vector is a Palindrome." << "\n";
     else
-        cout << "\nThis string vector is not a Palindrome." << endl;
-    
+        std::cout << "\nThis std::string std::vector is not a Palindrome." << "\n";
+
     return;
 }
--- Vector/vectorNotes.cpp
+++ Vector/vectorNotes.cpp
@@ -3,14 +3,14 @@
  * \brief  When the vector is full, it creates a new one with 50% more
  * space and move(or copy) the original data.
  * Note that the memory is allocated in a contiguous manner.
- * 
+ *
  * Despite the fact objects copy mechanism when stored in vector,
  * remember to provide a sanitized copy constructor.
- * 
+ *
  * Less commonly-seen member function of <vector>:
  * 1) .reserve(capacity)
  * 2) .emplace_back(elem) is a better approach to avoid copying
- * 
+ *
  * \author Xuhua Huang
  * \date   February 2021
  *********************************************************************/
@@ -21,8 +21,6 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
 namespace Util {
     namespace vector {
 
@@ -31,39 +29,41 @@ namespace Util {
         static void print_vec(const std::vector<T>& arg_vec) {
             /* Loop through the vector with range-based for loop. */
             for (const T& t : arg_vec)
-                std::cout << t << std::endl;
+                std::cout << t << std::"\n";
 
             return; // add empty return statement for consistency.
         }
     }
 }
 
-void printVector(vector<double> dv);
+void printVector(std::vector<double> dv);
 void func(double value);
 
 int main(void)
 {
+    using namespace std;
+
     /* Using template constructor */
-    vector<int> v1(6,0); // create an array of 6 elements and initialize them to zero
-    cout << v1.at(0) << endl; // display zero from position indexed 0
-    
+    vector<int> v1(6, 0); // create an array of 6 elements and initialize them to zero
+    cout << v1.at(0) << "\n"; // display zero from position indexed 0
+
     v1.push_back(9); // adds one element and sets it to 9
-    cout << v1.size() << endl;
-    
-    v1.resize(10,0); // add three more zeros to the vector
-    
+    cout << v1.size() << "\n";
+
+    v1.resize(10, 0); // add three more zeros to the vector
+
     v1.at(8) = 7;
-    cout << v1.at(8) << endl;
-    
-    
+    cout << v1.at(8) << "\n";
+
+
     // int vector iterator
     vector<int>::iterator iterint;
-    for(iterint = v1.begin();iterint < v1.end();iterint++)
+    for (iterint = v1.begin(); iterint < v1.end(); iterint++)
     {
-        cout << *iterint << endl;
+        cout << *iterint << "\n";
     }
-    
-    
+
+
     // double vector
     vector<double> dv; // a vector containing double using default constructor
     dv.push_back(1.2);
@@ -71,51 +71,51 @@ int main(void)
     dv.push_back(5.6);
     dv.push_back(7.8);
     dv.push_back(9.9);
-    
+
     vector<double>::iterator iterdouble;
-    for(iterdouble = dv.begin();iterdouble < dv.end(); iterdouble++)
+    for (iterdouble = dv.begin(); iterdouble < dv.end(); iterdouble++)
     {
-        cout << *iterdouble << endl;
+        cout << *iterdouble << "\n";
     }
-    
+
     /*
     dv.clear(); // wipes the entire array
     vector<double>::iterator iterdouble2;
     for(iterdouble2 = dv.begin();iterdouble2 < dv.end(); iterdouble2++)
     {
-        cout << *iterdouble << endl;
+        cout << *iterdouble << "\n";
     }
     */
-    
-    
+
+
     // ALGORITHMS
     sort(dv.begin(), dv.end()); // sort function
     printVector(dv);
-    
+
     reverse(dv.begin(), dv.end()); // reverse function
     printVector(dv);
-    
+
     // "for_each" - calls function func and pass each element in vector
-    for_each(dv.begin(),dv.end(),func);
-    
+    for_each(dv.begin(), dv.end(), func);
+
     // counts the number of times the element shows up
     int c = count(dv.begin(), dv.end(), 9.9);
-    cout << c << endl;
-    
+    cout << c << "\n";
+
     system("pause");
     return 0;
 }
 
 
-void printVector(vector<double> dv)
+void printVector(std::vector<double> dv)
 {
     /* Constant references could not be used here
      * due the nature of an iterator.
      */
-    vector<double>::iterator iterdouble;
-    for(iterdouble = dv.begin();iterdouble < dv.end(); ++iterdouble)
+    std::vector<double>::iterator iterdouble;
+    for (iterdouble = dv.begin(); iterdouble < dv.end(); ++iterdouble)
     {
-        cout << *iterdouble << endl;
+        std::cout << *iterdouble << "\n";
     }
 
     return;
@@ -124,8 +124,8 @@ void printVector(vector<double> dv)
 
 void func(double value)
 {
-    cout << "Inside  function func(): ";
-    cout << value << endl;
+    std::cout << "Inside  function func(): ";
+    std::cout << value << "\n";
 
     return;
 }
