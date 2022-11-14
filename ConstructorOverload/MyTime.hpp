aa69b18f6f8c5a1b2253aee3f9fe5204500240cd
XuhuaHuang <xuhua.huang.io@gmail.com>
2022-11-14 14:08:59 -0500

    Renamed folders to singular.

--- ConstructorOverload/MyTime.hpp
+++ ConstructorOverload/MyTime.hpp
@@ -1,16 +1,51 @@
 /*****************************************************************//**
- * \file   myclass_functions.cpp
- * \brief  contains function definitions
+ * \file   MyTime.hpp
+ * \brief  Class MyTime declaration and definition.
  * 
  * \author Xuhua Huang
  * \date   March 2021
  *********************************************************************/
 
-#include "LabUtilityClasses.h"
+#ifndef MYTIME_HPP
+#define MYTIME_HPP
 
-using namespace std;
+#ifndef _IOSTREAM_
+#include <iostream>
+#endif
 
-// MyTime class function
+class MyTime
+{
+private:
+    int hour;
+    int minute;
+    int second;
+
+public:
+    // set functions
+    void setHour(int userHour); // definiton of set functions follow with validation
+    void setMinute(int userMinute);
+    void setSecond(int userSecond);
+
+    // get functions
+    int getHour() { return hour; }
+    int getMinute() { return minute; }
+    int getSecond() { return second; }
+
+    // other functions within the class
+    void setTime(int userHour, int userMinute, int userSecond);
+    void printTime();
+
+    // overloaded functions with the same name and different arguments
+    void setTime(int userHour, int userMinnute);
+    void setTime(int userHour);
+
+    // default and overloaded constructor
+    MyTime(); // default
+    MyTime(int, int, int); // overloaded
+    MyTime(int, int);
+    MyTime(int);
+};
+
 MyTime::MyTime() // default constructor
 {
     setHour(0);
@@ -39,7 +74,7 @@ MyTime::MyTime(int h)
     setSecond(0); // set second to 0 by default
 }
 
-void MyTime::setHour (int userHour) // hour should be valid form 0 to 23
+void MyTime::setHour(int userHour) // hour should be valid form 0 to 23
 {
     if ((userHour >= 0) && (userHour <= 23))
     {
@@ -47,19 +82,19 @@ void MyTime::setHour (int userHour) // hour should be valid form 0 to 23
     }
     else if (userHour == 24)
     {
-        cout << "Do you mean 0 a.m.? It is automatically set to 0 a.m." << endl;
+        std::cout << "Do you mean 0 a.m.? It is automatically set to 0 a.m." << "\n";
         hour = 0;
     }
     else
     {
-        cout << "Invalid value " << userHour << endl;
+        std::cout << "Invalid value " << userHour << "\n";
         hour = 0;
     }
 
     return;
 }
 
-void MyTime::setMinute (int userMinute) // minute should be valid from 0 to 59
+void MyTime::setMinute(int userMinute) // minute should be valid from 0 to 59
 {
     if ((userMinute >= 0) && (userMinute <= 59))
     {
@@ -67,22 +102,22 @@ void MyTime::setMinute (int userMinute) // minute should be valid from 0 to 59
     }
     else
     {
-        cout << "Invalid value " << userMinute << endl;
+        std::cout << "Invalid value " << userMinute << "\n";
         minute = 0;
     }
 
     return;
 }
 
-void MyTime::setSecond (int userSecond) // second should be valid form 0 to 59
+void MyTime::setSecond(int userSecond) // second should be valid form 0 to 59
 {
-    if ((userSecond >= 0) && (userSecond<= 59))
+    if ((userSecond >= 0) && (userSecond <= 59))
     {
         second = userSecond;
     }
     else
     {
-        cout << "Invalid value " << userSecond << endl;
+        std::cout << "Invalid value " << userSecond << "\n";
         second = 0;
     }
 
@@ -119,47 +154,10 @@ void MyTime::setTime(int userHour) // overloaded function #2
 
 void MyTime::printTime()
 {
-    cout << getHour() << ":" << getMinute() << ":" << getSecond() << endl; // using get functions to get value with verification
+    // using get functions to get value with verification
+    std::cout << getHour() << ":" << getMinute() << ":" << getSecond() << "\n";
 
     return;
 }
 
-
-//Rectangle class function
-Rectangle::Rectangle() // default constructor the main will call when creating an object
-{
-    setlength(0);
-    setwidth(0);
-}
-
-Rectangle::Rectangle(int inilength, int iniwidth) // overloaded with two arguments passed from the main
-{
-    setlength(inilength);
-    setwidth(iniwidth);
-}
-
-int Rectangle::calcArea(int inilength, int iniwidth)
-{
-    int area = inilength * iniwidth;
-    return area;
-}
-
-int Rectangle::calcPerimeter(int inilength, int iniwidth)
-{
-    int perimeter = ( inilength + iniwidth ) *2;
-    return perimeter;
-}
-
-
-// Point class function
-Point::Point() // default cinstructor // constructors always have the same name with the class
-{
-    setx(0); // calling the set functions with argument 0
-    sety(0);
-}
-
-Point::Point(int iniX, int iniY)
-{
-    setx(iniX);
-    sety(iniY);
-}
+#endif
