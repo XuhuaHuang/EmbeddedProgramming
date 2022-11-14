7ef0abf80334f065727bfaf91819cdfc871d10de
XuhuaHuang <xuhua.huang.io@gmail.com>
2022-11-14 09:04:38 -0500

    Renamed and reformatted code.

---
+++ Miscellaneous/reverse_digits.cpp
@@ -0,0 +1,31 @@
+/*****************************************************************//**
+ * \file   reverse_digits.cpp
+ * \brief  g++ reverse_digits.cpp -o reverse_digits.exe
+ * ./reverse_digits.exe
+ *
+ * \author Xuhua Huang
+ * \date   November 06, 2020
+ *********************************************************************/
+
+#include <iostream>
+#include <cmath>
+
+int main(void)
+{
+    int value = 0;
+    std::cout << "Reversing a four digit number..." << "\n";
+    std::cout << "Please enter a four digits number: ";
+    std::cin >> value;
+
+    /* reading original digits
+    * int thousands = (value / 1000 % 10);
+    * int hundreds = (value / 100 % 10);
+    * int tens = (value / 10 % 10);
+    * int ones = (value % 10);
+    */
+
+    int reverseValue = (value % 10) * 1000 + (value / 10 % 10) * 100 + (value / 100 % 10) * 10 + (value / 1000 % 10) * 1;
+    std::cout << "The reversed value is now: " << reverseValue << "\n";
+
+    return 0;
+}
