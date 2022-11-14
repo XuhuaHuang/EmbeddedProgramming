48257dba5b5f27d680085245b4ca14a95c065e36
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-11-14 08:58:45 -0500

    Refactorred code in Miscellaneous folder.

--- Miscellaneous/hello_world.cpp
+++ Miscellaneous/hello_world.cpp
@@ -1,28 +1,35 @@
+/*****************************************************************//**
+ * \file   hello_world.cpp
+ * \brief  g++ -c hello_world.cpp -o hello_world.exe
+ * ./hello_world.exe
+ *
+ * \author Xuhua Huang
+ * \date   November 14, 2020
+ *********************************************************************/
+
 #include <windows.h>
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main(void)
 {
-	string greeting = "Hello, world!";
+    std::string greeting = "Hello, world!";
 
-	for (int iter = 0; iter < greeting.length(); iter++)
-	{
-		cout << greeting[iter];
-		Sleep(500);
-	}
-	
-	cout << " "; // an extra space to accommodate with the following for loop
-	Sleep(1500);
+    for (int iter = 0; iter < greeting.length(); iter++)
+    {
+        std::cout << greeting[iter];
+        Sleep(500);
+    }
 
-	for (int iter2 = 0; iter2 < greeting.length() ; iter2++)
-	{
-		cout << "\b\b ";
-		//greeting.replace(iter2, 1, " ");
-		Sleep(500);
-	}
+    std::cout << " "; // an extra space to accommodate with the following for loop
+    Sleep(1500);
 
-	return 0;
+    for (int iter2 = 0; iter2 < greeting.length(); iter2++)
+    {
+        std::cout << "\b\b ";
+        //greeting.replace(iter2, 1, " ");
+        Sleep(500);
+    }
+
+    return 0;
 }
