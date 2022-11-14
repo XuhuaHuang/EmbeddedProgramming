74a53217a85fe0fd7e5a601d9db832217f5e5b52
XuhuaHuang <xuhua.huang.io@gmail.com>
2022-11-14 12:31:57 -0500

    Reformatted files under ChangeTextColour directory.

--- ChangeTextColour/ChangeTextColour.c
+++ ChangeTextColour/ChangeTextColour.c
@@ -3,43 +3,42 @@
 
 int main()
 {
-    /*
-    HANDLE is a Windows API datatype
-    It's a pointer to a "handle".
-    Handles, and the many other similar types in Windows, are generally pointers to
-    files and the like.
-    In this case, when you write a C program, you have 3 "files" already opened for you:
-
-    stdout
-    stdin
-    stderr
-
-    You may not realize you're using them. When you use printf(), you're writing to stdout.
-    When you use scanf(), you're reading from stdin.
-    The code gets the HANDLE associated with stdout.
-
-    */
-
+    /**
+     * HANDLE is a Windows API datatype
+     * It's a pointer to a "handle".
+     * Handles, and the many other similar types in Windows, are generally pointers to
+     * files and the like.
+     * In this case, when you write a C program, you have 3 "files" already opened for you:
+     *
+     * stdout
+     * stdin
+     * stderr
+     *
+     * You may not realize you're using them. When you use printf(), you're writing to stdout.
+     * When you use scanf(), you're reading from stdin.
+     * The code gets the HANDLE associated with stdout. * .
+     */
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    /*
-    This is a WORD datatype variable that is going to be used to store the old data color info.
-    If you change the color of the console text in your application,
-    when your program is terminated, the color will not revert back to what it was before.
-    That is your job to set it back to normal.*/
+    /**
+     * This is a WORD datatype variable that is going to be used to store the old data color info.
+     * If you change the color of the console text in your application,
+     * when your program is terminated, the color will not revert back to what it was before.
+     * That is your job to set it back to normal..
+     */
     WORD wOldColorAttrs;
 
     CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
 
-    /*
-    * First save the current color information
-    */
+    /**
+     * First save the current color information.
+     */
     GetConsoleScreenBufferInfo(h, &csbiInfo);
     wOldColorAttrs = csbiInfo.wAttributes;
 
-    /*
-    * Set the new color information
-    */
+    /**
+     * Set the new color information
+     */
     SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_INTENSITY);
 
     printf("This is a test\n");
@@ -52,9 +51,9 @@ int main()
 
     printf("This is a test\n");
 
-    /*
-    * Restore the original colors
-    */
+    /**
+     * Restore the original colors.
+     */
     SetConsoleTextAttribute(h, wOldColorAttrs);
 
     printf("This is a test\n");
