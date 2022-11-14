31cec3ec8160c658e2bba6f1197b81087c2291ad
XuhuaHuang <xuhuahuang0412@gmail.com>
2022-11-14 12:22:32 -0500

    Reformatted GoogleTestDemo to independent directory with CMake.

---
+++ GoogleTest/hello_test.cpp
@@ -0,0 +1,17 @@
+/*****************************************************************//**
+ * \file   hello_test.cpp
+ * \brief  Testing GoogleTest project/directory setup.
+ * 
+ * \author Xuhua Huang
+ * \date   November 14, 2022
+ *********************************************************************/
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
