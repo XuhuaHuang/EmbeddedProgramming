e78f8240a30a2c6d480e911f4fe9a78cdfc08fb3
XuhuaHuang <xuhua.huang.io@gmail.com>
2022-11-26 19:19:22 -0500

    Moved files and added CMake.

--- Util/tests/test_parse_str.cpp
+++ Util/tests/test_parse_str.cpp
@@ -1,11 +1,11 @@
 /*****************************************************************//**
- * \file   demo_parse_str.cpp
+ * \file   test_parse_str.cpp
  * \brief  Demonstration of function to parse a comma-separated string
  * into integers and store them in a std::vector<int>.
  * 
  * To compile and run this file on Windows:
- * $ g++ -o demo_parse_str.exe .\demo_parse_str.cpp -std=c++11
- * $ .\demo_parse_str.exe
+ * $ g++ -o test_parse_str.exe .\test_parse_str.cpp -std=c++11
+ * $ .\test_parse_str.exe
  * 
  * \author Xuhua Huang
  * \date   October 2021
@@ -17,20 +17,19 @@
 #include <sstream>
 
 // Customized utility library
-#include "../Util/parse.h"
-#include "../Util/print_vec.hpp"
+#include "parse.hpp"
+#include "print_vec.hpp"
 
 int main() {
-
     /* Split a comma-separated string to an array of integers. */
     std::string str;
     // std::cin >> str;
 
-    str = "1,23,456,789";                       // demo with a comma as delimiter
+    str = "1,23,456,789";                       // test with a comma as delimiter
     std::vector<int> integers = util::parse::parse_str<int>(str);
     util::vector::print_vec<int>(integers);
 
-    std::string cell_phone = "541-952-1655";    // demo with a dash as delimiter
+    std::string cell_phone = "541-952-1655";    // test with a dash as delimiter
     std::vector<int> cell_to_ints = util::parse::parse_str<int>(cell_phone, '-');
     util::vector::print_vec<int>(cell_to_ints);
 
@@ -40,5 +39,5 @@ int main() {
     util::vector::print_vec<std::string>(spell_name);
 
     system("pause");
-    return 0;
+    return EXIT_SUCCESS;
 }
--- Util/tests/test_safe_free.cpp
+++ Util/tests/test_safe_free.cpp
@@ -1,6 +1,10 @@
 /*****************************************************************//**
- * \file   safe_free.cpp
- * \brief  
+ * \file   test_safe_free.cpp
+ * \brief  Demonstration of library code in ".\util\safe_free.hpp"
+ * 
+ * To compile and run this file on Windows:
+ * $ g++ -o test_safe_free.exe .\test_safe_free.cpp -std=c++11
+ * $ .\test_safe_free.exe
  * 
  * \author Xuhua Huang
  * \date   September 2022
@@ -8,7 +12,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <Util\safe_free.hpp>
+#include "safe_free.hpp"
 
 int main(void)
 {
@@ -22,5 +26,5 @@ int main(void)
     util::pointer::safe_free((void**)&(pi));
 
     system("pause");
-    return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
