5d7aa0d5c12d4669b91259ad95ff14dbed5d396d
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-01-07 10:46:35 -0500

    Removed using namespace declaration.

--- Inheritance/Inheritance_notes.cpp
+++ Inheritance/Inheritance_notes.cpp
@@ -11,6 +11,9 @@
  * Derived Class 'Cat'
  * default / overloaded constructors
  * function definitions
+ * 
+ * $ g++ -c Inheritance_notes.cpp -o notes.exe -std=c++11
+ * $ .\notes
  *
  * @version 1.0
  * @date November 2020
@@ -23,25 +26,23 @@
 #include <cmath>
 #include <string>
 
-using namespace std;
-
 class Pet
 {
 private: // protected if tired of set*() and get*()
-    string name;
+    std::string name;
     int age;
 
 public:
     // default and overloaded constructors
     Pet();
-    Pet(int newage, string newname);
+    Pet(int newage, std::string newname);
 
     // set function:
-    void setName(string newname) { name = newname; }
+    void setName(std::string newname) { name = newname; }
     void setAge(int newage) { age = newage; }
 
     // get function:
-    string getName() const { return name; }
+    std::string getName() const { return name; }
     int getAge() const { return age; }
 
 };
@@ -49,16 +50,16 @@ public:
 class Dog : public Pet
 {
 private:
-    string breed;
+    std::string breed;
 
 public:
     // default and overloaded constructor
     Dog();
-    Dog(string newbreed, int newage, string newname); // : Pet(newage, newname) { setbreed(newbreed); setAge(getAge() * 7); }
+    Dog(std::string newbreed, int newage, std::string newname); // : Pet(newage, newname) { setbreed(newbreed); setAge(getAge() * 7); }
 
     // get and set function for new element string breed:
-    void setBreed(const string newbreed) { breed = newbreed; }
-    string getBreed() const { return breed; }
+    void setBreed(const std::string newbreed) { breed = newbreed; }
+    std::string getBreed() const { return breed; }
 };
 
 class Cat : public Pet
@@ -69,7 +70,7 @@ private:
 public:
     // default and overloaded constructor
     Cat();
-    Cat(int newnumLives, int newage, string newname); // : Pet(newage, newname)
+    Cat(int newnumLives, int newage, std::string newname); // : Pet(newage, newname)
 
     // get and set functions for new element int numLives:
     void setnumLives(const int newNumLives) { numLives = newNumLives; }
@@ -82,12 +83,12 @@ int main(void)
     Dog dog1("golden retriever", 5, "doggy doggy"); // calling overloaded constructor, should aged 35
     Cat cat1;
 
-    cout << "Pet1 age is: " << pet1.getAge() << endl << "Pet1 name is: " << pet1.getName() << endl;
-    cout << "\nDog1 age is: " << dog1.getAge() << endl << "Dog1 name is: " << dog1.getName() << " of breed: " << dog1.getBreed() << endl;
-    cout << "\nCat1 age is: " << cat1.getAge() << endl << "Cat1 name is: " << cat1.getName() << endl;
+    std::cout << "Pet1 age is: " << pet1.getAge() << "\n" << "Pet1 name is: " << pet1.getName() << "\n";
+    std::cout << "\nDog1 age is: " << dog1.getAge() << "\n" << "Dog1 name is: " << dog1.getName() << " of breed: " << dog1.getBreed() << "\n";
+    std::cout << "\nCat1 age is: " << cat1.getAge() << "\n" << "Cat1 name is: " << cat1.getName() << "\n";
 
     cat1.setnumLives(8);
-    cout << "\nCat1 has this amount of lives: " << cat1.getnumLives() << endl;
+    std::cout << "\nCat1 has this amount of lives: " << cat1.getnumLives() << "\n";
 
     return 0;
 }
@@ -98,7 +99,7 @@ Pet::Pet() // default constructor for the Pet class
     setName("");
 }
 
-Pet::Pet(int newage, string newname) // overloaded constructor for the Pet class
+Pet::Pet(int newage, std::string newname) // overloaded constructor for the Pet class
 {
     setAge(newage);
     setName(newname);
@@ -115,7 +116,7 @@ Dog::Dog() // default constructor for Dog class
 * reminder: Pet(int, string) itself is a overloaded constructor
 * 'string newbreed' is the new element preserved for class 'Dog'
 */
-Dog::Dog(string newbreed, int newage, string newname) : Pet(newage, newname)
+Dog::Dog(std::string newbreed, int newage, std::string newname) : Pet(newage, newname)
 {
     setBreed(newbreed);
     setAge(getAge() * 7); // set the human perspective age
@@ -132,7 +133,7 @@ Cat::Cat() // default constructor for Cat class
 * reminder: Pet(int, string) itself is a overloaded constructor
 * 'int newnumLives' is the new element preserved for class 'Cat'
 */
-Cat::Cat(int newnumLives, int newage, string newname) : Pet(newage, newname)
+Cat::Cat(int newnumLives, int newage, std::string newname) : Pet(newage, newname)
 {
     setnumLives(newnumLives);
 }
