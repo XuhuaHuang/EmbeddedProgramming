be1e273d23eccd4c5d371972fc3c4c4c88aedb77
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-01-08 17:14:00 -0500

    Uploaded WindowsMLDesktopApp source files.

---
+++ WindowsMLDesktopApp/WindowsMLDesktopApp/pch.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <winrt/Windows.AI.MachineLearning.h>
+#include <winrt/Windows.Graphics.Imaging.h>
+#include <winrt/Windows.Media.h>
+#include <winrt/Windows.Storage.h>
+
+#include <string>
+#include <fstream>
+
+#include <Windows.h>
