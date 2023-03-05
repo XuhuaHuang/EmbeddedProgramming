4764eb2aa9941ce3d6ab45cd9184b9f6e2e85d82
XuhuaHuang <xuhuahuang0412@gmail.com>
2023-03-05 14:12:35 -0500

    Update search_insert_position.py

--- LeetCodePlayground/search_insert_position.py
+++ LeetCodePlayground/search_insert_position.py
@@ -1,24 +1,42 @@
 # https://leetcode.com/problems/search-insert-position/
 
 from typing import List
+import unittest
+
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        low: int  = 0
+        low: int = 0
         high: int = len(nums)
-        mid: int  = 0
+        mid: int = 0
 
         if target > nums[high - 1]:
             return high
 
-        while (low <= high):
-            mid = (low + high)
-            if (nums[mid] == target):
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
                 return mid
 
-            if (target < nums[mid]):
+            if target < nums[mid]:
                 high = mid - 1
             else:
                 low = mid + 1
 
         return low
+
+
+class TestSearchInsertPositionSolution(unittest.TestCase):
+
+    def test_solution(self):
+        nums: List[int] = [1,3,5,6]
+        solution: Solution = Solution()
+        # Input: nums = [1,3,5,6], target = 5
+        # Output: 2
+        assert(2 == solution.searchInsert(nums=nums, target=5))
+        assert(1 == solution.searchInsert(nums=[1, 3, 5, 6], target=2))
+        assert(4 == solution.searchInsert(nums=[1, 3, 5, 6], target=7))
+
+
+if __name__ == '__main__':
+    unittest.main()
