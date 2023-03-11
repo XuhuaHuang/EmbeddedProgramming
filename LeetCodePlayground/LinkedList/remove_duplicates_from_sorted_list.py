5c85e9b493e02bf50924f9bad7e48a7eabf65209
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-03-10 19:13:46 -0500

    Moved to LinkedList folder

--- LeetCodePlayground/LinkedList/remove_duplicates_from_sorted_list.py
+++ LeetCodePlayground/LinkedList/remove_duplicates_from_sorted_list.py
@@ -1,4 +1,4 @@
-# 
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -8,10 +8,12 @@
 
 from pyparsing import Optional
 
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
@@ -20,7 +22,7 @@ class Solution:
         else:
             head_bkup: ListNode = head
             while head.next is not None:
-                if (head.val == head.next.val):
+                if head.val == head.next.val:
                     head.next = head.next.next
                 else:
                     head = head.next
