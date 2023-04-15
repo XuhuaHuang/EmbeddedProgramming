a59c6d9628cdefd4d6a28ccdeeca4e5da43c6d82
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-04-15 18:37:08 -0400

    Create movable_array_arithmetic.hpp

---
+++ Helper/movable_array_arithmetic.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef MOVABLE_ARRAY_ARITHMETIC_HPP
+#define MOVABLE_ARRAY_ARITHMETIC_HPP
+
+#include <array>
+#include <concepts>
+#include <iostream>
+
+#endif // !MOVABLE_ARRAY_ARITHMETIC_HPP
