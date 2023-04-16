a3c4b23e77a29f985fec65fbbaf9cc0c26092446
XuhuaHuang <xuhuahuang0412@gmail.com>
2023-04-15 21:00:28 -0400

    Create doctest_basic_example.cpp

---
+++ Helper/doctest_basic_example.cpp
@@ -0,0 +1,9 @@
+// cl .\doctest_basic_example.cpp /W4 /Zc:__cplusplus /EHsc /std:c++latest /experimental:module
+// .\doctest_basic_example.exe
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.hpp"
+
+#include <iostream>
+#include <vector>
+
