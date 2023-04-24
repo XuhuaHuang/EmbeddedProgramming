5a68815125716a65dbc8f2eb3097b24b2523c1a6
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-04-24 15:30:00 -0400

    Update ranges_notes.cpp and person.hpp

--- PointerToImpl/person.hpp
+++ PointerToImpl/person.hpp
@@ -19,15 +19,16 @@
 namespace pimpl
 {
 
-class Person {
+class Person
+{
 public:
     Person(const std::string& name, int age);
     ~Person();
 
     std::string name() const;
-    void setName(const std::string& name);
+    void        setName(const std::string& name);
 
-    int age() const;
+    int  age() const;
     void setAge(int age);
 
 private:
@@ -35,34 +36,45 @@ private:
     std::unique_ptr<PersonImpl> impl_;
 };
 
-class Person::PersonImpl {
+class Person::PersonImpl
+{
 public:
     std::string name;
-    int age;
+    int         age;
 };
 
 Person::Person(const std::string& name, int age)
-    : impl_(new PersonImpl{ name, age })
-{}
+    : impl_(new PersonImpl{name, age})
+{
+}
 
 Person::~Person() = default;
 
-std::string Person::name() const {
+std::string Person::name() const
+{
     return impl_->name;
 }
 
-void Person::setName(const std::string& name) {
+void Person::setName(const std::string& name)
+{
     impl_->name = name;
 }
 
-int Person::age() const {
+void Person::setName(std::string&& name)
+{
+    impl_->name = std::move(name);
+}
+
+int Person::age() const
+{
     return impl_->age;
 }
 
-void Person::setAge(int age) {
+void Person::setAge(int age)
+{
     impl_->age = age;
 }
 
-}
+} // namespace pimpl
 
 #endif
