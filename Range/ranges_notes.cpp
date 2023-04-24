dabc04407e384f6241e51a6b2856544b8667e699
XuhuaHuang <xuhuahuang0412@gmail.com>
2023-04-24 15:30:00 -0400

    Update ranges_notes.cpp and person.hpp

--- Range/ranges_notes.cpp
+++ Range/ranges_notes.cpp
@@ -1,3 +1,4 @@
+// clang-format off
 /*****************************************************************//**
  * \file   ranges_notes.cpp
  * \brief  Demonstration of ranges and views in C++ 20.
@@ -11,57 +12,59 @@
  * \author Xuhua Huang
  * \date   March 2022
  *********************************************************************/
+// clang-format on
 
-#include <iostream>
 #include <iomanip>
-#include <vector>
+#include <iostream>
 #include <ranges>
-
-// #include "Util/print_range.hpp"
-// util::range::print_range()
+#include <type_traits>
+#include <vector>
 
 int main()
 {
-    std::vector<std::int16_t> vec { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::vector<std::int16_t> vec{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     constexpr auto is_even = [&](int elem) -> bool { return elem % 2 == 0; };
-    constexpr auto square = [&](int elem) -> int { return elem * elem; };
+    constexpr auto square  = [&](int elem) -> int { return elem * elem; };
 
     /* with std::views::filter */
-    auto even_nums = vec | std::views::filter(is_even);
+    auto even_nums         = vec | std::views::filter(is_even);
     // view on stack with adapter
-    for (const int16_t& i : even_nums) {
-       std::cout << i << " ";
+    for (const auto& i : even_nums)
+    {
+        std::cout << i << " ";
     }
     std::cout << "\n"
-       << "End execution with " << std::quoted("std::views::filter(is_even)") << "\n";
+              << "End execution with " << std::quoted("std::views::filter(is_even)") << "\n";
 
     /* with std::ranges::filter_view */
-    for (const int16_t& i : std::ranges::filter_view(vec, is_even)) {
-       std::cout << i << " ";
+    for (const auto& i : std::ranges::filter_view(vec, is_even))
+    {
+        std::cout << i << " ";
     }
     std::cout << "\n"
-       << "End execution with " << std::quoted("std::ranges::filter_view(vec, is_even)") << "\n";
+              << "End execution with " << std::quoted("std::ranges::filter_view(vec, is_even)") << "\n";
 
     /* with std::views::transform */
     auto even_nums_squared = even_nums | std::views::transform(square);
     // view on range-for loop
-    for (const int16_t& i : even_nums_squared) {
-       std::cout << i << " ";
+    for (const auto& i : even_nums_squared)
+    {
+        std::cout << i << " ";
     }
     std::cout << "\n"
-       << "End execution with " << std::quoted("std::views::transform(square)") << "\n";
+              << "End execution with " << std::quoted("std::views::transform(square)") << "\n";
 
     /* pipelining syntax */
-    auto output = vec
-       | std::views::filter(is_even)
-       | std::views::transform(square);
+    auto output = vec | std::views::filter(is_even) | std::views::transform(square);
     // view on range-for loop
-    for (const int16_t& i : output) {
-       std::cout << i << " ";
+    for (const auto& i : output)
+    {
+        std::cout << i << " ";
     }
     std::cout << "\n"
-       << "End execution with pipeline syntax" << "\n";
+              << "End execution with pipeline syntax"
+              << "\n";
 
     return 0;
 }
