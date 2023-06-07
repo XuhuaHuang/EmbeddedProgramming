85e6a91e6187bf3123eb3d23967f3165cbca7773
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-06-07 09:28:53 -0400

    Update is_instance_of.hpp

--- Util/is_instance_of.hpp
+++ Util/is_instance_of.hpp
@@ -1,3 +1,4 @@
+// clang-format off
 /*****************************************************************//**
  * \file   is_instance_of.hpp
  * \brief  Implementation of popular Python function isinstance()
@@ -9,22 +10,26 @@
  * \author Xuhua Huang
  * \date   October 2021
  *********************************************************************/
+// clang-format on
 
 #ifndef IS_INSTANCE_OF_HPP
 #define IS_INSTANCE_OF_HPP
 
 #include <iostream>
 
-namespace util {
-    namespace type {
+namespace util
+{
+namespace type
+{
 
-        /* Template to determine the type of parsed object. */
-        template<typename Base, typename T>
-        inline bool is_instance_of(const T&) {
-            return std::is_base_of<Base, T>::value;
-        }
+/* Template to determine the type of parsed object. */
+template<typename Base, typename T>
+inline bool is_instance_of(const T&)
+{
+    return std::is_base_of<Base, T>::value;
+}
 
-    } // end util::type
-} // end util
+} // namespace type
+} // namespace util
 
 #endif
