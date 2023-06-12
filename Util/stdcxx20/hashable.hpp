5d798d311ba30cdac257cae5aacac898b1014d0c
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-06-11 23:02:51 -0400

    Format file

--- Util/stdcxx20/hashable.hpp
+++ Util/stdcxx20/hashable.hpp
@@ -5,21 +5,25 @@
  * to determine whether an object is 'hashable'.
  * @version 0.1
  * @date 2022-01-27
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include <concepts> // require C++20 standard
 
-namespace util {
-    namespace type_safety {
+namespace util
+{
+namespace type_safety
+{
 
-        /* Templated concept to determine whether can call hash function on an object. */
-        template<typename T>
-        concept hashable = requires(T t) {
-            { std::hash<T>{}(t) } -> std::convertible_to<std::size_t>;
-        }
+/* Templated concept to determine whether can call hash function on an object. */
+template<typename T>
+concept hashable = requires (T t) {
+    {
+        std::hash<T>{}(t)
+    } -> std::convertible_to<std::size_t>;
+}
 
-    } // end type_safety
-} // end util
+} // namespace type_safety
+} // namespace util
