6da7b9e4e0cdeea4f76683e17417cc33f6cba424
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-06-14 09:25:10 -0400

    Create raw_ranges_to_vector_unittest.cpp

---
+++ Range/raw_ranges_to_vector_unittest.cpp
@@ -0,0 +1,13 @@
+#include "boost/ut.hpp"
+// https://github.com/boost-ext/ut
+#include "raw_ranges_to_vector.hpp"
+
+#include <list>
+#include <range/v3/range.hpp>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/take.hpp>
+#include <ranges>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
