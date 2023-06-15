e52995ec8370bb2bc9c494a479bce0f72d648cf0
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-06-15 12:17:15 -0400

    Create named_tuple_unittest.cpp

---
+++ Util/named_tuple_unittest.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file named_tuple_unittest.cpp
+ * @author Xuhua Huang
+ * @brief
+ * @version 0.1
+ * @date 2023-06-14
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "named_tuple.hpp"
+
+int main(int argc, const char** argv)
+{
+    return 0;
+}
