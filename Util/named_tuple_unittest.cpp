b60a7f7c19043126299c1b5fe1921b4386accbac
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-06-18 18:27:38 -0400

    Update named_tuple_unittest.cpp

--- Util/named_tuple_unittest.cpp
+++ Util/named_tuple_unittest.cpp
@@ -9,9 +9,36 @@
  *
  */
 
+// https://github.com/boost-ext/ut
+#include "boost/ut.hpp"
 #include "named_tuple.hpp"
 
 int main(int argc, const char** argv)
 {
+    using namespace boost::ut;
+    using util::type::namedtuple;
+
+    "named tuple"_test = [] {
+        should("allow empty") = [] {
+            const auto nt = namedtuple{};
+            expect(not [](auto t) { return requires { t[""_t]; }; }(nt));
+        };
+
+        should("support direct initialization") = [] {
+            const auto nt = namedtuple{"price"_t = 42, "size"_t = 100};
+            expect([](auto t) { return requires { t["price"_t]; }; }(nt));
+            expect([](auto t) { return requires { t["size"_t]; }; }(nt));
+            expect(not [](auto t) { return requires { t["quantity"_t]; }; }(nt));
+            expect(42_i == nt["price"_t] and 100_i == nt["size"_t]);
+        };
+
+        should("support modification") = [] {
+            auto nt       = namedtuple{"price"_t = int{}, "size"_t = std::size_t{}};
+            nt["price"_t] = 12;
+            nt["size"_t]  = 34u;
+            expect(12_i == nt["price"_t] and 34_u == nt["size"_t]);
+        };
+    };
+
     return 0;
 }
