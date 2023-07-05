9996673d1ae38e681de68885e80d07a00ac493b8
XuhuaHuang <xuhuahuang0412@gmail.com>
2023-07-05 18:19:31 -0400

    Create ostream_joiner.hpp

---
+++ Util/ostream_joiner.hpp
@@ -0,0 +1,12 @@
+#ifndef OSTREAM_JOINER_HPP
+#define OSTREAM_JOINER_HPP
+
+namespace util
+{
+namespace data_structure
+{
+
+} // namespace data_structure
+} // namespace util
+
+#endif // !OSTREAM_JOINER_HPP
