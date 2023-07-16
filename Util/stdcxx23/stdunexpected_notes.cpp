4a794c60adbfadfa236676cafff12b5f77093c33
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-07-16 16:00:43 -0400

    Create stdunexpected_notes.cpp

---
+++ Util/stdcxx23/stdunexpected_notes.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file stdunexpected_notes.cpp
+ * @author Xuhua Huang
+ * @brief Notes on working with std::unexpected in C++23.
+ *
+ * cl .\stdunexpected_notes.cpp /std:c++latest /EHsc
+ * .\stdunexpected_notes
+ *
+ * g++ stdunexpected_notes.cpp -std=c++2b -o stdunexpected_notes -Wall -Wextra -O3 -Wno-pragma-once-outside-header
+ *
+ * @version 0.1
+ * @date 2023-07-15
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
