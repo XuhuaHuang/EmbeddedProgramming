c2160b7b36b43408a775f6f1ee677696f911cbdf
XuhuaHuang <xuhuahuang0412@gmail.com>
2023-07-20 11:32:28 -0400

    Add g++ compile command to bind_back

--- VariadicTemplateParameter/bind_back.hpp
+++ VariadicTemplateParameter/bind_back.hpp
@@ -1,6 +1,9 @@
 // cl .\bind_back.hpp /W4 /Zc:__cplusplus /EHsc /std:c++latest /exportHeader
 // output: bind_back.hpp.ifc
 
+// g++ .\bind_back.hpp -std=c++2b -Wall -Wextra -O3 -Wno-pragma-once-outside-header
+// output: bind_back.hpp.gch
+
 #include <type_traits>
 
 [[nodiscard]] constexpr auto bind_back(auto&& fn, auto&&... back_args)
