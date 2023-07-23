5febf1caa0ba4444accf1cb2983df74fd314e77c
XuhuaHuang <xuhuahuang0412@gmail.com>
2023-07-23 18:14:58 -0400

    Create sum.hpp

---
+++ Span/sum.hpp
@@ -0,0 +1,29 @@
+/**
+ * @file sum.hpp
+ * @author Xuhua Huang
+ * @brief
+ * @version 0.1
+ * @date 2023-07-23
+ *
+ * @copyright ueg (c) 2023
+ *
+ */
+
+#ifndef SUM_HPP
+#define SUM_HPP
+
+namespace util
+{
+namespace span
+{
+
+// clang-format off
+constexpr auto sum = []([[maybe_unused]] auto s) -> auto {
+    return std::accumulate(std::cbegin(s), std::cend(s), 0);
+};
+// clang-format on
+
+} // namespace span
+} // namespace util
+
+#endif // !SUM_HPP
