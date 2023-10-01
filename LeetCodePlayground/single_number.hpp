5625c4ba334232ad15d9908e00ffab19dc7aafd1
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-09-30 20:21:30 -0400

    Create single_number.hpp

---
+++ LeetCodePlayground/single_number.hpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/single-number
+
+class Solution
+{
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i += 2)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                return nums[i - 1];
+            }
+        }
+        return nums[nums.size() - 1];
+    }
+};
