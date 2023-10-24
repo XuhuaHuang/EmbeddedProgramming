56c8bf1ff8d06cb349d284cd5fe07bead379a400
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-10-23 21:17:01 -0400

    Create matrix_determinant.hpp

---
+++ IntroductionToRobotics/matrix_determinant.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file matrix_determinant.hpp
+ * @author Xuhua Huang
+ * @brief
+ * @version 0.1
+ * @date 2023-10-23
+ *
+ * Recall that for any matrix A, det(A Transposed) == det(A)
+ * For any matrices A and B that can multiplied, det(AB) == det(A)det(B).
+ *
+ * @copyright ueg (c) 2023
+ *
+ */
+
+#pragma once
+#ifndef MATRIX_DETERMINANT_HPP
+#define MATRIX_DETERMINANT_HPP
+
+#include <array>
+#include <concepts>
+#include <type_traits>
+
+#endif // !MATRIX_DETERMINANT_HPP
