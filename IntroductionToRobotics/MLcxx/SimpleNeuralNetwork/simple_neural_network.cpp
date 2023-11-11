67e1d6b824cad8f2bcc99886cad3ee947c82c393
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-11-10 20:27:30 -0500

    Create simple_neural_network.cpp

---
+++ IntroductionToRobotics/MLcxx/SimpleNeuralNetwork/simple_neural_network.cpp
@@ -0,0 +1,14 @@
+/*****************************************************************//**
+ * \file   simple_neural_network.cpp
+ * \brief  Define and train a simple neural network on the MNIST dataset.
+ * 
+ * \author Xuhua Huang
+ * \date   November 10, 2023
+ *********************************************************************/
+
+#include <torch/torch.h>
+
+int main()
+{
+    return 0;
+}
