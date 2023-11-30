5586e346ff261bdd0defe67a11baadba923f9a2b
XuhuaHuang <xuhua.huang.io@gmail.com>
2023-11-30 17:32:52 -0500

    Create main.cpp

---
+++ Util/physics/main.cpp
@@ -0,0 +1,18 @@
+/* clang-format off */
+/*****************************************************************//**
+ * \file   main.cpp
+ * \brief  Projectile motion calculation use case and static assertions.
+ * 
+ * \author Xuhua Huang
+ * \date   January 31, 2023
+ *********************************************************************/
+/* clang-format on */
+
+#include <iostream>
+
+#include "projectile_motion.hpp"
+
+auto main(void) -> int
+{
+    return EXIT_SUCCESS;
+}
