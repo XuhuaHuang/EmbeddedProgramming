349bddfee783515fdb36d337044cdf2d7d2db61a
XuhuaHuang <xuhua.huang.io@gmail.com>
2024-03-03 12:48:49 -0500

    Create is_type_unittest.cpp

---
+++ Helper/is_type_unittest.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file is_type_unittest.cpp
+ * @author Xuhua Huang
+ * @brief
+ * @version 0.1
+ * @date 2024-03-03
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+#include <type_traits>
+
+#include "is_type.hpp"
+
+int main(void)
+{
+    return 0;
+}
