3de92f3ca3b6b9dba8de8662ef49f37471fd65ac
XuhuaHuang <xuhua.huang.io@gmail.com>
2024-03-17 21:27:46 -0400

    Create lambda_derivative.hpp

---
+++ Helper/lambda_derivative.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file lambda_derivative.hpp
+ * @author Xuhua Huang
+ * @brief
+ * @version 0.1
+ * @date 2024-03-17
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
+
+#ifndef LAMBDA_DERIVATIVE_HPP
+#define LAMBDA_DERIVATIVE_HPP
+
+namespace util
+{
+namespace lambda_derivative
+{
+
+using float32_t = std::float_t;
+
+} // namespace lambda_derivative
+} // namespace util
+
+#endif // !LAMBDA_DERIVATIVE_HPP
