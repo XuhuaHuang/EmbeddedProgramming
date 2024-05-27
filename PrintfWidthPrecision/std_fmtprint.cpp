0636d21838ab9756c7a71dd33cbf823822f1cb69
XuhuaHuang <xuhuahuang0412@gmail.com>
2024-05-26 21:03:45 -0400

    Update print folder and project CMake list

--- PrintfWidthPrecision/std_fmtprint.cpp
+++ PrintfWidthPrecision/std_fmtprint.cpp
@@ -1,28 +1,30 @@
-﻿/*****************************************************************//**
+﻿// clang-format off
+/*****************************************************************//**
  * \file   std_fmtprint.cpp
  * \brief  
  * 
  * \author Xuhua Huang
  * \date   December 20, 2022
  *********************************************************************/
+// clang-format on
 
 #include <array>
 #include <cctype>
-#include <format>
-#include <ranges>
-#include <numbers>
-#include <iostream>
 #include <cstdlib>
-#include <array>
 #include <filesystem>
+#include <format>
+#include <iostream>
+#include <numbers>
 #include <print>
+#include <ranges>
 
-auto demo_stdprint_tofile() -> void {
+auto stdprint_tofile() -> void
+{
     std::print("{0} {2}{1}!\n", "Hello", 23, "C++"); // overload (1)
 
-    const auto tmp{ std::filesystem::temp_directory_path() / "test.txt" };
+    const auto tmp{std::filesystem::temp_directory_path() / "test.txt"};
 
-    if (std::FILE * stream{ std::fopen(tmp.c_str(), "w") })
+    if (std::FILE * stream{std::fopen(tmp.c_str(), "w")})
     {
         std::print(stream, "File: {}", tmp.string()); // overload (2)
         std::fclose(stream);
@@ -31,38 +33,38 @@ auto demo_stdprint_tofile() -> void {
     return;
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
+    std::println("{} arguments passed to main! Hello, world!", argc);
+    std::println("{}", argv[0]);
+
     std::array<char, 24> buf;
     std::format_to(buf.begin(), "{:.15f}", std::numbers::sqrt2);
 
     unsigned num{}, sum{};
 
+    // clang-format off
     auto v = buf
         | std::views::filter(isdigit)
         | std::views::transform([](char x) { return x - '0'; })
-        | std::views::take_while([&sum](char) { return sum < 42; })
-        ;
+        | std::views::take_while([&sum](char) { return sum < 42; });
+    // clang-format on
 
-    for (auto n : v) {
+    for (auto n : v)
+    {
         sum += n;
         ++num;
     }
 
 #ifdef __cpp_lib_print
-    std::print(
-        std::cout,
-        "√2 = {}...\n"
-        "The sum of its first {} digits is {}{}\n",
-        std::numbers::sqrt2, num, sum, '.'
-    );
+    std::print(std::cout,
+               "√2 = {}...\n"
+               "The sum of its first {} digits is {}{}\n",
+               std::numbers::sqrt2, num, sum, '.');
 #else
-    std::cout <<
-        std::format(
-            "√2 = {}...\n"
-            "The sum of its first {} digits is {}{}\n",
-            std::numbers::sqrt2, num, sum, '.'
-        );
+    std::cout << std::format("√2 = {}...\n"
+                             "The sum of its first {} digits is {}{}\n",
+                             std::numbers::sqrt2, num, sum, '.');
 #endif
 
     return EXIT_SUCCESS;
--- PrintfWidthPrecision/std_printf.cpp
+++ PrintfWidthPrecision/std_printf.cpp
@@ -1,3 +1,4 @@
+// clang-format off
 /*****************************************************************//**
  * \file   std_printf.cpp
  * \brief
@@ -5,31 +6,31 @@
  * \author Xuhua Huang
  * \date   December 20, 2022
  *********************************************************************/
+// clang-format on
 
-#include <cstdio>
-#include <limits>
-#include <cstdint>
 #include <cinttypes>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <limits>
 
 int main()
 {
     const char* s = "Hello";
-    std::printf("Strings:\n"); // same as std::puts("Strings:");
-    std::printf("\t[%10s]\n",          s); // 10 characters, allign right
-    std::printf("\t[%-10s]\n",         s); // - sign, allign left
-    std::printf("\t[%*s]\n",    10,    s);
-    std::printf("\t[%-10.*s]\n", 4,    s); // 10 chars, - sign allign left, prints 4 chars
+
+    std::printf("Strings:\n");     // same as std::puts("Strings:");
+    std::printf("\t[%10s]\n", s);  // 10 characters, allign right
+    std::printf("\t[%-10s]\n", s); // - sign, allign left
+    std::printf("\t[%*s]\n", 10, s);
+    std::printf("\t[%-10.*s]\n", 4, s); // 10 chars, - sign allign left, prints 4 chars
     std::printf("\t[%-*.*s]\n", 10, 4, s);
 
     std::printf("Characters:\t%c %%\n", 'A');
 
     std::printf("Integers:\n");
-    std::printf("\tDecimal:    \t%i %d %.6i %i %.0i %+i %i\n",
-        1, 2, 3, 0, 0, 4, -4);
-    std::printf("\tHexadecimal:\t%x %x %X %#x\n",
-        5, 10, 10, 6);
-    std::printf("\tOctal:      \t%o %#o %#o\n",
-        10, 10, 4);
+    std::printf("\tDecimal:    \t%i %d %.6i %i %.0i %+i %i\n", 1, 2, 3, 0, 0, 4, -4);
+    std::printf("\tHexadecimal:\t%x %x %X %#x\n", 5, 10, 10, 6);
+    std::printf("\tOctal:      \t%o %#o %#o\n", 10, 10, 4);
 
     std::printf("Floating point:\n");
     std::printf("\tRounding:\t%f %.0f %.32f\n", 1.5, 1.5, 1.3);
@@ -45,8 +46,7 @@ int main()
 
     std::printf("Fixed-width types:\n");
     std::uint32_t val = std::numeric_limits<std::uint32_t>::max();
-    std::printf("\tLargest 32-bit value is %" PRIu32 " or %#" PRIx32 "\n",
-        val, val);
+    std::printf("\tLargest 32-bit value is %" PRIu32 " or %#" PRIx32 "\n", val, val);
 
     return EXIT_SUCCESS;
 }
