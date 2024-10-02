eeb739bfefe86ad000dfceb7841236ed74e74db8
XuhuaHuang <xuhua.huang.io@gmail.com>
2024-10-01 20:34:04 -0400

    Update assignment.cpp

--- OperatorOverload/assignment.cpp
+++ OperatorOverload/assignment.cpp
@@ -1,3 +1,4 @@
+// clang-format off
 /*****************************************************************//**
  * \file   overload_assignment.cpp
  * \brief  Overloaded assignment operator 
@@ -5,19 +6,30 @@
  * \author Xuhua
  * \date   November 2020
  *********************************************************************/
+// clang-format on
 
 #include <iostream>
 
+/**
+ * @class point
+ * @brief A class representing a point in 2D space.
+ * 
+ * The point class encapsulates the coordinates of a point in a 2D space with integer values.
+ * It provides default constructors, a copy constructor, and a virtual destructor.
+ * 
+ * @note This class uses public member variables for the coordinates.
+ */
 class point
 {
 public:
-    constexpr point() = default;
+    constexpr point()                 = default;
     constexpr point(const point& rhs) = default;
-    virtual ~point() = default;
+    virtual ~point()                  = default;
 
-    int _x; int _y;
+    int _x;
+    int _y;
 
-    point& operator= (const point&);
+    point& operator=(const point&);
 };
 
 point& point::operator=(const point& otherPoint)
