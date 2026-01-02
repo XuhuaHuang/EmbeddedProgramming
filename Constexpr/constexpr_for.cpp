0918ec59abe1d18ae3d2373afb86b26e73cdf8dc
XuhuaHuang <xuhuahuang0412@gmail.com>
2026-01-02 22:18:28 +0000

    Change to constexpr recursion

--- Constexpr/constexpr_for.cpp
+++ Constexpr/constexpr_for.cpp
@@ -11,17 +11,11 @@
 
 #include <iostream>
 
-constexpr int factorial(int n) {
-    int result = 1;
-    for (int i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
-}
+constexpr int factorial(int n) { return n <= 1 ? 1 : n * factorial(n - 1); }
 
 constexpr int f = factorial(5);
 
 int main() {
-    static_assert(f == 120, "Factorial of 5 should be 120");
-    return 0;
+  static_assert(f == 120, "Factorial of 5 should be 120");
+  return 0;
 }
