2a258313870b33c6e62dbac40ed256d852321391
XuhuaHuang <xuhuahuang0412@gmail.com>
2026-01-15 14:50:58 +0000

    Update notes on smart pointers

--- Pointer/smart_ptr.cpp
+++ Pointer/smart_ptr.cpp
@@ -1,3 +1,4 @@
+// clang-format off
 /*****************************************************************//**
  * \file   smart_pointers.cpp
  * \brief  contains notes on smart pointers in C++
@@ -9,36 +10,53 @@
  * \author Xuhua Huang
  * \date   May 2021
  *********************************************************************/
+// clang-format on
 
+#include <cassert>
 #include <iostream>
 #include <memory>
 
 class Entity {
 public:
-    Entity() { std::cout << "[DFCONST]Creating Entity..." << "\n"; }
+  inline Entity() { std::cout << "[DFCONST] Creating Entity..." << "\n"; }
+  inline ~Entity() { std::cout << "[DFDEST] Destroying Entity..." << "\n"; }
 
-    ~Entity() { std::cout << "[DFDEST]Destroying Entity..." << "\n"; }
-
-    void printEntity() { std::cout << "[fn]Printing Entity..." << "\n"; }
+  inline void printEntity() { std::cout << "[fn] Printing Entity..." << "\n"; }
 };
 
 int main(void) {
-    /**
-     * Create a new scope and verify the output in the terminal.
-     * Expecting constructor, destructor get called sequentially.
-     * \return
-     */
-    {
-        std::unique_ptr<Entity> entity_ptr(new Entity()); // not exception safe
-        // better approach: make_unique<Entity>
-        // equivalent to: std::unique_ptr<Entity> entity_ptr = std::make_unique<Entity>();
-        entity_ptr->printEntity(); // using pointer as how normally it is used
-    } // destructor gets called automatically when pointer goes out of scope
+  /**
+   * Create a new scope and verify the output in the terminal.
+   * Expecting constructor, destructor get called sequentially.
+   * \return
+   */
+  {
+    std::unique_ptr<Entity> entity_ptr(new Entity()); // not exception safe
+    // better approach: make_unique<Entity>
+    // equivalent to: std::unique_ptr<Entity> entity_ptr =
+    // std::make_unique<Entity>();
+    entity_ptr->printEntity(); // using pointer as how normally it is used
+  } // destructor gets called automatically when pointer goes out of scope
 
-    // share_ptr uses reference count approach to determine when to delete the pointer
-    std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
-    std::weak_ptr<Entity>   weakEntity   = sharedEntity; // does not increase reference count
+  {
+    // reference counted pointer
+    // shared pointers are traditionally initialized to be null by the compiler
+    // when declared if they are not assigned something on creation
+    // when the reference count drops to zero, the pointer gets deleted
+    // automatically
+    std::shared_ptr<int> sp = nullptr;
+    sp = std::make_shared<int>(); // exception safe
+    assert(sp != nullptr);
+    assert(*sp == 0); // default initialized to 0
+    *sp = 1;
+    assert(*sp == 1);
+    std::cout << "Value of shared pointer: " << *sp << "\n";
+  }
+  // share_ptr uses reference count approach to determine when to delete the
+  // pointer
+  std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
+  std::weak_ptr<Entity> weakEntity =
+      sharedEntity; // does not increase reference count
 
-    std::cin.get();
-    return 0;
+  return 0;
 }
