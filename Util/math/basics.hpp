3cf452581a8a838d8eb71ca0402652ca30fcda0f
XuhuaHuang <xuhuahuang0412@gmail.com>
2026-01-18 21:05:30 +0000

    Reformat basics.hpp

--- Util/math/basics.hpp
+++ Util/math/basics.hpp
@@ -24,46 +24,46 @@ namespace math {
 template <typename T>
 [[nodiscard]]
 inline T mean(const auto& data)
-    requires std::ranges::range<decltype(data)>
+  requires std::ranges::range<decltype(data)>
 {
-    return std::accumulate(std::begin(data), std::end(data), static_cast<T>(0)) / data.size();
+  return std::accumulate(std::begin(data), std::end(data), static_cast<T>(0)) / data.size();
 }
 
 template <typename T>
 [[nodiscard]]
 inline T std_deviation(const auto& data) {
-    T mean    = std::accumulate(std::begin(data), std::end(data), static_cast<T>(0)) / data.size();
-    T sq_sum  = std::inner_product(data.begin(), data.end(), data.begin(), static_cast<T>(0));
-    T std_dev = std::sqrt(sq_sum / data.size() - static_cast<T>(mean * mean));
-    return std_dev;
+  T mean    = std::accumulate(std::begin(data), std::end(data), static_cast<T>(0)) / data.size();
+  T sq_sum  = std::inner_product(data.begin(), data.end(), data.begin(), static_cast<T>(0));
+  T std_dev = std::sqrt(sq_sum / data.size() - static_cast<T>(mean * mean));
+  return std_dev;
 }
 
 template <typename Iterable>
 bool any(Iterable&& iterable) {
-    return std::any_of(std::begin(iterable), std::end(iterable), [](const auto& x) { return static_cast<bool>(x); });
+  return std::any_of(std::begin(iterable), std::end(iterable), [](const auto& x) { return static_cast<bool>(x); });
 }
 
 template <typename InputIt, typename UnaryPredicate>
 bool any(InputIt first, InputIt last, UnaryPredicate pred) {
-    for (; first != last; ++first) {
-        if (pred(*first)) {
-            return true;
-        }
+  for (; first != last; ++first) {
+    if (pred(*first)) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 bool all(const auto& data) {
-    bool true_so_far = true;
-    for (const auto& d : data) {
-        true_so_far = d && true;
-        if (!true_so_far) [[likely]] {
-            return false;
-        } else {
-            continue;
-        }
+  bool true_so_far = true;
+  for (const auto& d : data) {
+    true_so_far = d && true;
+    if (!true_so_far) [[likely]] {
+      return false;
+    } else {
+      continue;
     }
-    return true_so_far;
+  }
+  return true_so_far;
 }
 
 } // namespace math
