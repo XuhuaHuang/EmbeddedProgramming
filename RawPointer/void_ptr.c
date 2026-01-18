9029f7554d0deb81c4884a8e1d2e54e8ae528466
XuhuaHuang <xuhuahuang0412@gmail.com>
2026-01-18 19:41:54 +0000

    Update notes on void pointer

--- RawPointer/void_ptr.c
+++ RawPointer/void_ptr.c
@@ -17,37 +17,37 @@
 #include <stdlib.h>
 
 int main(void) {
-    void* vp;
-    int*  ip;
+  void* vp;
+  int*  ip;
 
-    /* print out the size of pointers */
-    printf("sizeof(void*) -> %zu\n", sizeof(vp));
-    printf("sizeof(int*) -> %zu\n", sizeof(ip));
+  /* print out the size of pointers */
+  printf("sizeof(void*) -> %zu\n", sizeof(vp));
+  printf("sizeof(int*) -> %zu\n", sizeof(ip));
 
-    int x = 0xFEEDBEEF;
-    ip    = &x;
-    vp    = ip;
+  int x = 0xFEEDBEEF;
+  ip    = &x;
+  vp    = ip;
 
-    /* print out the address of both p and ip */
-    printf("vp -> %p\n", vp);
-    printf("ip -> %p\n", ip);
+  /* print out the address of both p and ip */
+  printf("vp -> %p\n", vp);
+  printf("ip -> %p\n", ip);
 
-    /* dereference the int* and assign it a new value */
-    *ip = 0x00C0FFEE;
-    //*p = 0xDEADBEEF; // void is not assignable. no type information
-    *(int*)vp = 0xDEADBEEF; // this assignment will overwrite the 0xCOFFEE assignment
-    // because both *p and *ip point to the same address
-    // therefore, they both read the latest assignment
+  /* dereference the int* and assign it a new value */
+  *ip = 0x00C0FFEE;
+  //*p = 0xDEADBEEF; // void is not assignable. no type information
+  *(int*)vp = 0xDEADBEEF; // this assignment will overwrite the 0xCOFFEE assignment
+  // because both *p and *ip point to the same address
+  // therefore, they both read the latest assignment
 
-    /* print out hexadecimal value */
-    printf("vp -> %p, with value of %x\n", vp, *(int*)vp);
-    printf("ip -> %p with value of %x\n", ip, *ip);
+  /* print out hexadecimal value */
+  printf("vp -> %p, with value of %x\n", vp, *(int*)vp);
+  printf("ip -> %p with value of %x\n", ip, *ip);
 
-    /* c and cpp differ with the following lines */
-    // int* ip2 = malloc(sizeof(int)); // error in cpp, requires explicit conversion
-    // prefer the explicit conversion for portability and readability
-    int* ip2 = (int*)malloc(sizeof(int));
-    free(ip2);
+  /* c and cpp differ with the following lines */
+  // int* ip2 = malloc(sizeof(int)); // error in cpp, requires explicit conversion
+  // prefer the explicit conversion for portability and readability
+  int* ip2 = (int*)malloc(sizeof(int));
+  free(ip2);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
---
+++ RawPointer/void_ptr.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file void_ptr.cpp
+ * @author Xuhua Huang
+ * @brief
+ * @version 0.1
+ * @date 2026-01-18
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+  std::nullptr_t np = nullptr;
+  void*          vp = nullptr;
+  int*           ip = nullptr;
+
+  // print out the size of pointers
+  std::cout << "sizeof(void*) -> " << sizeof(vp) << std::endl;
+  std::cout << "sizeof(int*) -> " << sizeof(ip) << std::endl;
+
+  int x = 0xFEEDBEEF;
+  ip    = &x;
+  vp    = ip;
+
+  // print out the address of both p and ip
+  std::cout << "vp -> " << vp << std::endl;
+  std::cout << "ip -> " << ip << std::endl;
+
+  // dereference the int* and assign it a new value
+  *ip = 0x00C0FFEE;
+  // *p = 0xDEADBEEF; // void is not assignable. no type information
+  *(static_cast<int*>(vp)) = 0xDEADBEEF; // this assignment will overwrite the 0xCOFFEE assignment
+  // because both *p and *ip point to the same address
+  // therefore, they both read the latest assignment
+
+  // print out hexadecimal value
+  std::cout << "vp -> " << vp << ", with value of " << std::hex << *(static_cast<int*>(vp)) << std::endl;
+  std::cout << "ip -> " << ip << " with value of " << std::hex << *ip << std::endl;
+
+  // c and cpp differ with the following lines
+  // int* ip2 = malloc(sizeof(int)); // error in cpp, requires explicit conversion
+  // prefer the explicit conversion for portability and readability
+  int* ip2 = static_cast<int*>(std::malloc(sizeof(int)));
+  std::free(ip2);
+
+  return 0;
+}
